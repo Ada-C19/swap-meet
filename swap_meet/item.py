1f5d48f181cbe2c03c015d83636ea7d231fbe061
Jennifer Tam <jenniferwtam@gmail.com>
2023-04-03 13:22:07 -0700

    Wave 2 passed all tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,16 @@
 import uuid
 
 class Item:
-    def __init__(self, id = int(uuid.uuid4())):
-        self.id = id
-
+    def __init__(self, id = None):
+        '''
+        If id does not exist, assign unique id with uuid
+        Otherwise, self.id = id
+        '''
+        if not id:
+            self.id = int(uuid.uuid4())
+        else:
+            self.id = id
     
+    # Function named `get_category` will return a string holding the name of the class
     def get_category(self):
-        pass
+        return self.__class__.__name__
