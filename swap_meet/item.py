4ae8e4647983d4864d00cc007c2dbbb87b3f8179
Sarah Watkins <sarahwwatkins@gmail.com>
2023-04-03 14:31:30 -0400

    Wave 3 done

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -4,6 +4,8 @@ class Item:
     def __init__(self, id=None):
         self.id = uuid1().int if id is None else id
 
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
 
     def get_category(self):
         return f"{self.__class__.__name__}"
