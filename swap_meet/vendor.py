653848bb0dea47434abb54b6228449fe1c16ba3e
Sydney Alberson <sydney.alberson@gmail.com>
2023-04-03 13:25:15 -0400

    Small changes to class Vendor and removed skips on test wave 02

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,9 @@
 # In Wave 1 we will create the Vendor class.
 #     There is a module (file) named vendor.py inside of the swap_meet package (folder)
 #     Inside this module, there is a class named Vendor
-#     Each Vendor will have an attribute named inventory, which is an empty list by default
 #     When we instantiate an instance of Vendor, we can optionally pass in a list with the keyword argument inventory
 #     Every instance of Vendor has an instance method named add, which takes in one item
-#     This method adds the item to the inventory
-#     This method returns the item that was added
 #     Similarly, every instance of Vendor has an instance method named remove, which takes in one item
-#     This method removes the matching item from the inventory
-#     This method returns the item that was removed
-#     If there is no matching item in the inventory, the method should return False
 
 
 class Vendor:
@@ -19,16 +13,22 @@ class Vendor:
     # word_list.append(word)
     # return word_list
 
+#     Each Vendor will have an attribute named inventory, which is an empty list by default
     def __init__(self,inventory=None):
         self.inventory = inventory if inventory is not None else []
 
     def add(self, item):
+#     This method adds the item to the inventory
         self.inventory.append(item)
+#     This method returns the item that was added
         return item
     
     def remove(self, item):
+#     If there is no matching item in the inventory, the method should return False
         if item not in self.inventory:
             return False
         
+#     This method removes the matching item from the inventory
         self.inventory.remove(item)
+#     This method returns the item that was removed
         return item
