efcb751e8c3d0f547604a035ce102f293a184926
Trang Tran <trantrang181197@gmail.com>
2023-04-03 16:06:52 -0700

    class Clothing passed

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,18 @@
+import uuid
+
 class Clothing:
-    pass
+    def __init__(self, id=None, fabric="Unknown"):
+        if not id:
+            self.id = uuid.uuid1().int
+        else:
+            self.id = id
+
+        self.fabric = fabric
+    
+    def get_category(self):
+        return "Clothing"
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+        
+    
