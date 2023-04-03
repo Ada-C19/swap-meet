50f7765cffd1ca925c3b7918021d9e6b34be374e
Winslow <owedwards@gmail.com>
2023-04-03 13:39:15 -0400

    Completed class Decor

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,11 @@
 class Decor:
-    pass
+    def __init__(self, id, width = 0, length = 0):
+        self.id = id
+        self.width = width
+        self.length = length
+
+    def get_category(self):
+        return "Decor"
+
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}. It takes up a {self.width} by {self.length} sized space."
