6bfdc483c94a009a5f341632a1d3d1f6d48f1dfb
Susi Franco <susi.franco32@gmail.com>
2023-04-03 15:47:16 -0700

    I created the decor class inheriting from the item class.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,21 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+class Decor(Item):
+    def __init__(self, id=None, width=0, length=0):
+        if id is None:
+            super().__init__()
+        else:
+            super().__init__(id=id)
+
+        if width is None:
+            self.width = 0.0
+        else: 
+            self.width = width
+            
+        if length is None:
+            self.length = 0.0
+        else:
+            self.length = length
+
+    def __str__(self):
+        return f"An object of type {self.__class__.__name__} with id {self.id}. It takes up a {self.width} by {self.length} sized space."
+    
