18963366bc1c97dbbac131e380731cf118a0804f
Sydney Alberson <sydney.alberson@gmail.com>
2023-04-03 14:35:30 -0400

    Completed wave 02

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,13 @@
+import uuid
+
 class Item:
     # In Wave 2 we will create the Item class and the Vendor class' get_by_id method.
     # There is a module (file) named item.py inside of the swap_meet package (folder)
      # Inside this module, there is a class named Item
     # Each Item will have an attribute named id, which is a unique integer by default
-    # There are many ways to generate numbers, but generating numbers without duplicates takes some care. Happily, Python has a package called uuid that can help!
-   # If we import the uuid package in item.py, with a little research we can use one of the functions uuid provides to create large unique numbers meant to be used as identifiers
+    # There are many ways to generate numbers, but generating numbers without duplicates takes some care. 
+    # Happily, Python has a package called uuid that can help!
+   # With a little research we can use one of the functions uuid provides to create large unique numbers meant to be used as identifiers
     # Specifically, you'll need to choose which of the uuid package's functions to use, so be sure to consider which function will work best for creating a unique integer
     # Note that this package's functions return UUID objects, not integers as such, but UUID objects have an attribute int which allow us to access their value as an integer
      # When we initialize an instance of Item, we can optionally pass in an integer with the keyword argument id to manually set the Item's id
@@ -13,3 +16,15 @@ class Item:
     # This method takes one argument: an integer, representing an Item's id
     # This method returns the item with a matching id from the inventory
     # If there is no matching item in the inventory, the method should explicitly return None
+
+
+
+    # def add_to_list_ok(word, word_list=None):
+    # word_list = word_list if word_list is not None else []
+    def __init__(self, id=None):
+        # self.item = item
+        self.id = id if id is not None else int(uuid.uuid4())
+
+    def get_category(self):
+        return "Item"
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -5,7 +5,6 @@
 #     Every instance of Vendor has an instance method named add, which takes in one item
 #     Similarly, every instance of Vendor has an instance method named remove, which takes in one item
 
-
 class Vendor:
 
     # def add_to_list_ok(word, word_list=None):
@@ -26,9 +25,15 @@ class Vendor:
     def remove(self, item):
 #     If there is no matching item in the inventory, the method should return False
         if item not in self.inventory:
-            return False
-        
+            return False 
 #     This method removes the matching item from the inventory
         self.inventory.remove(item)
 #     This method returns the item that was removed
         return item
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if id == item.id:
+                return item
+        
+        return None
