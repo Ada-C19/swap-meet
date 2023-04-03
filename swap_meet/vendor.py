4870b0de161a20e44ce3d525e4d5509c9a4746b6
Lyuda Kim <lyudarkim@gmail.com>
2023-04-03 15:44:28 -0700

    Passing unit test 3 but failing integration test. Wrote swap_items() in Vendor.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -19,3 +19,18 @@ class Vendor:
             if item.id == id:
                 return item
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            # Remove my_item form this Vendor's inventor 
+            item_1_to_swap = self.remove(my_item)
+            # Add it to the friend's inventory
+            other_vendor.add(item_1_to_swap)
+            # Remove their_item from other Vendor's inventory
+            item_2_to_swap = other_vendor.remove(their_item)
+            # add it to this Vendor's inventory
+            self.add(item_2_to_swap)
+            return True
+        # If item isn't in the inventory
+        return False
