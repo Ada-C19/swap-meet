1ab0f4a1f92deb0dba131710f72fd87b728d5aa7
La Tasha Pollard <latashajpollard@gmail.com>
2023-04-03 17:38:19 -0500

    passes first four tests in wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,10 @@
 import uuid 
 class Item:
-    def __init__(self, id= uuid.uuid4().int):
-        self.id = id
+    def __init__(self, id = uuid.uuid4().int):
+        self.id = uuid.uuid4().int
     
     def get_category(self):
         return "Item"
+    
+
+    
