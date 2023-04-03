66308f02956f57353c581c8a0cc1a6e580e2e099
Angie <caritocon@gmail.com>
2023-04-03 14:22:57 -0700

    Part of wave 2 passing

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,16 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        # If id is passed assign it to id
+        # If not create a new id
+        if id:
+            self.id = id
+        else:
+            self.id = uuid.uuid4().int
+    
+    def get_category(self):
+        # Holds the class name and returns it as a str
+        return self.__class__.__name__
+
+    
