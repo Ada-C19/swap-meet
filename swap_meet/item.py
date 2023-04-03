8242e150bd95bc69d18656dffdb0ffd4cc46c492
Elaine Watkins <ewatki.ga@gmail.com>
2023-04-03 13:12:55 -0400

    Completed tests for Wave 1, starting on Wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,14 @@
+import uuid 
+
+# We can use uuid.uuid4() which will return an object
+# We can then use an attribute
 class Item:
-    pass
+    def __init__(self, id):
+        self.id = id
+
+    def get_category(): 
+        pass
+        # return str of the name of the class
+
+# when initializing an instance of Item, can optionally pass in an intger with a keyword arg id to manually set id
+# item = Item(id)
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,25 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory # needs to be an empty list by default
+    
+    # adds single item to inventory and returns item added
+    def add(self, item):  
+        self.inventory.append(item)
+        return item
+    
+    # removes single item from invetory, returns item removed | if not matching item in inventory, method should return False
+    def remove(self, item): 
+        if item in self.inventory: 
+            self.inventory.remove(item)
+            return item
+        return False
+
+    # this is described as a function and not a method - why? 
+    def get_by_id(id):
+        pass    
+        # returns item with matching id from inventory
+        # if no matching item, then explicitly return None
+
+
+# instantiate a list and optionally pass in a list with keyword arg named inventory
+# vendor_example = Vendor(inventory=[])   
