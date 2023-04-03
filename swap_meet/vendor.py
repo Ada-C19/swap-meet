9e169d9befefb7100274c729a6ef3fdde6fd9bdd
Susi Franco <susi.franco32@gmail.com>
2023-04-03 14:55:10 -0700

    created a swap first item method in vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -20,17 +20,32 @@ class Vendor:
         return None
     
     def swap_items(self, other_vendor, my_item, their_item):
-        #if my item isn't in my inventory, 
+        #if my item not in my inventory, 
         #or their item not in their inventory return false.
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
         
-        #remove my item, add their item.
-        self.remove(my_item)
+        #add their item to self, my item to their inventory.
         self.add(their_item)
-        #for other vendor, remove their item, and add mine.
-        other_vendor.remove(their_item)
         other_vendor.add(my_item)
+
+        #remove my item from my inventory, their item from their inventory.
+        self.remove(my_item)
+        other_vendor.remove(their_item)
+        
 
         return True
 
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        #remove my first and their first
+        my_first = self.inventory.pop(0)
+        their_first = other_vendor.inventory.pop(0)
+
+        #add their first to my inventory, and my first to theirs
+        self.add(their_first)
+        other_vendor.add(my_first)
+
+        return True
+            
