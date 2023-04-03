76494f5ae8789286e6b23b43be1ea6487e13b799
Susi Franco <susi.franco32@gmail.com>
2023-04-03 13:31:39 -0700

    Created Vendor class with inventory attribute and two methods, one that adds item to inventory, and another that removes item from inventory

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
+from swap_meet.item import Item
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
