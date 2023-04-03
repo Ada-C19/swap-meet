b490c6db7cb03b414063c5750f7286db09703b21
Amanda <jonesamanda996@gmail.com>
2023-04-03 13:16:58 -0400

    Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,21 @@
+# vendor class attributes
+# init methos (self)
+# -keyword arg (optional) inventory(empty list)
+# add method (return item added to list)
+# remove method (return item removed from list)
+# if no matching item found return False
 class Vendor:
-    pass
+    # passing optional argument called inventory 
+    def __init__(self, inventory=None):
+        self.inventory = [] if inventory is None else inventory
+    # method that adds item to inventory 
+    def add(self, item):
+        if item:
+            self.inventory.append(item)
+        return item 
+    # method that removes item from inventory if in inventory
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item 
