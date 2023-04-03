c22145e97d45fd6e1184cfb170c4789ee646ab31
louloera <louloera@gmail.com>
2023-04-03 14:29:10 -0700

    Created class vendor with parameter inventory with methods to remove and add from it. Added Assert to terst for Wave1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self,inventory=[]):
+        self.inventory=inventory
+
+    def add(self,new_item):
+        self.inventory.append(new_item)
+        return new_item
+    
+    def remove(self, old_item):
+        if old_item in self.inventory:
+            self.inventory.remove(old_item)
+            return old_item
+        else: 
+            return False
