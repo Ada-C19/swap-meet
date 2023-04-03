c4780d435a50a11b31b7eb87cd2ad3af449dd7e8
Sage Choi <coolcuds@gmail.com>
2023-04-03 14:43:33 -0700

    Added constructor and instance methods to Vendor class.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+    
+    def add(self, item):
+        if item:
+            self.inventory.append(item.lower())
+        
+        return item
+
+    def remove(self, item):
+        try:
+            self.inventory.remove(item.lower())
+            return item
+        except ValueError as err:
+            print(f"Unable to remove '{item}' from inventory.")
+            return False
