0f44c8b9ff58b0e9a00bcea6cb32417159cbc069
Setharika Sok <soksetharika7@gmail.com>
2023-04-03 13:48:11 -0400

    Finished and pass wave 2 and test 02

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,10 @@
+import uuid
 class Item:
-    pass
+    def __init__(self,id = None):
+        id = uuid.uuid4().int if id is None else id
+        self.id = id
+    
+    def get_category(self):
+        return "Item"
+   
+        
