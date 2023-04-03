dcda23f764ca78580791695d02b2096007c44bc9
Monica <monicalagdaan@hotmail.com>
2023-04-03 11:48:00 -0700

    worked on some of wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,10 @@
 class Vendor:
-    pass
+    def __init(self, inventory)
+        self.inventory = []
+
+    def add(self, item)
+        self.inventory.append(item)
+        return item 
+
+    def remove(self):
+        
