d7cb0554ff5dbfefa794bfe36f9823c17d32307a
Jennifer Tam <jenniferwtam@gmail.com>
2023-04-03 16:05:28 -0700

    Passed all Wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -25,11 +25,20 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
-        
+
         self.inventory.remove(my_item)
         self.inventory.append(their_item)
         other_vendor.inventory.remove(their_item)
         other_vendor.inventory.append(my_item)
-    
+
         return True
     
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        else:
+            item_to_swap = self.inventory.pop(0)
+            other_item_to_swap = other_vendor.inventory.pop(0)
+            self.inventory.append(other_item_to_swap)
+            other_vendor.inventory.append(item_to_swap)
+        return True
