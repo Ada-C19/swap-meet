1f598f692c9989fd18a4d49d7a70a8660136edd2
Kaliane <vankaliane@gmail.com>
2023-04-03 16:58:02 -0400

    wave 3 complete

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -7,7 +7,7 @@ class Vendor:
         self.inventory =  inventory if inventory is not None else []
         
         
-        
+
     # 3. Every instance of Vendor has an instance method named add, 
     # which takes in one item
     def add(self, added_item):
@@ -31,7 +31,31 @@ class Vendor:
         else:
             return False
         
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
+
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        # self.other_vendor = other_vendor
+        self.my_item = my_item
+        self.their_item = their_item
+
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+
+        self.inventory.remove(my_item)
+        other_vendor.inventory.append(my_item)
+
+        other_vendor.inventory.remove(their_item)
+        self.inventory.append(their_item)
             
+        return True
+        
+
+
 
         
 
