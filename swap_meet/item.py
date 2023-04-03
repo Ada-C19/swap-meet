b281e4019fb40d537a7d0bb35afe0ff2a044b356
Kimberly Lantigua <klantigua11@gmail.com>
2023-04-03 15:03:26 -0700

    partially defined Item class (constructor, get_category). Passed 4/6 tests in test_wave_02

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
+import uuid
+
 class Item:
-    pass
+    
+    # item constructor 
+    def __init__(self, id = None):
+        if not id: 
+            id = uuid.uuid4().int
+        self.id = id
+    
+    # get by category method
+    def get_category(self): 
+        return "Item"
+    
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,13 +1,17 @@
 class Vendor:
     
     # constructor for the vendor class
-    def __init__(self, inventory = []): 
+    def __init__(self, inventory = None): 
+        if not inventory: 
+            inventory = []
         self.inventory = inventory
-
+    
+    # add method 
     def add(self, item): 
         self.inventory.append(item)
         return item
     
+    # remove method
     def remove(self, item): 
         if item in self.inventory: 
             self.inventory.remove(item)
