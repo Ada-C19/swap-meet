b9a52c831a5b09ed2eb80f884a3290b23bfb667e
Winslow <owedwards@gmail.com>
2023-04-03 13:36:25 -0400

    Completed class Clothing

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,10 @@
 class Clothing:
-    pass
+    def __init__(self, id, fabric = "Unknown"):
+        self.id = id
+        self.fabric = fabric
+
+    def get_category(self):
+        return "Clothing"
+
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}. It is made from {self.fabric} fabric."
