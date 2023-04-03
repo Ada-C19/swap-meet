7e9dd1e55f27d8a8f45f522a2909864ec38db273
Kaliane <vankaliane@gmail.com>
2023-04-03 17:33:59 -0400

    "updated to pass wave 3"

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,4 +2,13 @@ from swap_meet.vendor import Vendor
 import uuid
 
 class Item:
-    pass
+
+    def __init__(self, id=None):
+        self.id =  id if id is not None else uuid.uuid4().int
+
+    def get_category(self):
+        return self.__class__.__name__
+
+    def __str__(self):
+
+        return f"An object of type Item with id {self.id}."
