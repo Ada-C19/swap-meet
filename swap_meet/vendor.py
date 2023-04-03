d629a7d05285343a5626b4290e1792d98fd3ccc6
Stacey <stacey.l.joseph@gmail.com>
2023-04-03 13:16:22 -0400

    passing wave 1, test 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,41 @@
-class Vendor:
-    pass
+# 1. create the Vendor class
+class Vendor: 
+    
+    # 2. Vendor will have an attribute named inventory, 
+    # which is an empty list by default
+    def __init__(self, inventory=None):
+        self.inventory =  inventory if inventory is not None else []
+        
+        
+        
+    # 3. Every instance of Vendor has an instance method named add, 
+    # which takes in one item
+    def add(self, added_item):
+        self.added_item = added_item
+            
+        # 3a. This method adds the item to the inventory
+        self.inventory.append(added_item)
+        # 3b. This method returns the item that was added
+        return added_item
+        
+    # 4. every instance of Vendor has an instance method named remove, 
+    # which takes in one item
+    def remove(self, removed_item):
+        self.removed_item = removed_item
+            
+        # 4a. This method removes the matching item from the inventory
+        if removed_item in self.inventory: 
+            self.inventory.remove(removed_item)
+            # 4b. This method returns the item that was removed
+            return removed_item
+        else:
+            return False
+        
+            
+
+        
+
+
+
+
+
