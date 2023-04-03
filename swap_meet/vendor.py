da84aff5f377a99d65ce128f2976646084e2cce4
Sarah Kim <kimsuyon2013@gmail.com>
2023-04-03 15:22:08 -0700

    changed constructor attribute to have an option of having empty list vs a list

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,9 +1,14 @@
 class Vendor:
-    def __init__(self,inventory):
-        self.inventory = []
+    
+    def __init__(self,inventory = None):
+        if not inventory: 
+            inventory = []
+        self.inventory = inventory 
+
     def add (self, item):
         self.inventory.append(item)
         return item 
+    
     def remove (self, item):
         try:  
             self.inventory.remove(item)
