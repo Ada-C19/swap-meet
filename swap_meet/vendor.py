6ff289473ccdf52bcc68443f74996996f4c6009c
Trang Tran <trantrang181197@gmail.com>
2023-04-03 15:53:48 -0700

    shorten swap_items() code

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,8 +22,9 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
-        self.inventory.remove(my_item)
-        other_vendor.inventory.remove(their_item)
-        self.inventory.append(their_item)
-        other_vendor.inventory.append(my_item)
+        self.inventory[self.inventory.index(my_item)] = their_item
+        other_vendor.inventory[other_vendor.inventory.index(their_item)] = my_item
         return True
+    
+    #def swap_first_item(self, other_vendor):
+
