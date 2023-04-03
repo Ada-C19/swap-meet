2ee4081841a0520cdb0dc59cad8834f7b4fce943
Melinda Mott <melinda.mott@gmail.com>
2023-04-03 13:24:16 -0700

    Add and remove created

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,15 @@
 class Vendor:
-    """woaw"""
-    pass
+    def __init__(self):
+        self.inventory = []
+
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+        else: 
+            return False
+        
+        
