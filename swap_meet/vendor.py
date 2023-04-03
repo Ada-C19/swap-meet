fb204f905bdd8a1bc0acf346f5900454cfd2f985
Sage Choi <coolcuds@gmail.com>
2023-04-03 15:21:31 -0700

    Altered constructor for more pythonic reading.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,17 +1,20 @@
 class Vendor:
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=None):
+        if not inventory:
+            inventory = []
         self.inventory = inventory
     
     def add(self, item):
         if item:
-            self.inventory.append(item.lower())
+            self.inventory.append(item)
         
         return item
 
     def remove(self, item):
         try:
-            self.inventory.remove(item.lower())
+            self.inventory.remove(item)
             return item
         except ValueError as err:
+            print(f"Error message: {err}")
             print(f"Unable to remove '{item}' from inventory.")
             return False
