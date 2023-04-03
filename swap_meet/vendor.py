2faf06768021ef0c384ca5b786b8416ce9f82507
dreinicke27 <danicareinicke@gmail.com>
2023-04-03 11:40:59 -0700

    implemented and tested wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        
+        self.inventory.remove(item)
+        return item
+    
+    def get_by_id(self, id):
+        try:
+            for i in range(len(self.inventory)):
+                if self.inventory[i].id == id:
+                    return self.inventory[i]
+        except:
+            return None
