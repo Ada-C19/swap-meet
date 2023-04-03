577fde71d28ebb8de641373a505a1391b3db8fb2
Rainacam <rainacam12@gmail.com>
2023-04-03 17:28:38 -0400

    added functions

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,14 +1,56 @@
-import uuid 
+import uuid as uuid
 
 # We can use uuid.uuid4() which will return an object
 # We can then use an attribute
-class Item:
-    def __init__(self, id):
-        self.id = id
+# class Item:
+#     def __init__(self, id):
+#         self.id = id
 
-    def get_category(): 
-        pass
+#     def get_category(): 
+#         pass
         # return str of the name of the class
+
+class Item:
+    def __init__(self, category="", condition=0.0, id=None):
+        self.category = category
+        self.condition = condition
+        # if there is no id
+        if id is None:
+            # generate uniquie id as an int
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+    # Each `Item` will have function named `get_category`
+    def get_category(self):
+        # return a string holding the name of the class
+        return self.__class__.__name__
+
 
 # when initializing an instance of Item, can optionally pass in an intger with a keyword arg id to manually set id
 # item = Item(id)
+
+    # this is described as a function and not a method - why? 
+    def get_by_id(self, item_id):
+        # for each item in my inventory
+        for item_id in self.inventory:
+            # if id in question matches any item_id i have,
+            if item_id == item_id:
+        # return item with matching id from inventory
+                return item_id
+            # if no matching item, then return None
+            return None  
+        
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        # if i have item to give and they have their item to give 
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            # remove item from my list to...
+            self.remove(my_item)
+            # add to the other vendor's list then...
+            other_vendor.add(my_item)
+            # removes their item to give from their list
+            other_vendor.remove(their_item)
+            # and adds their item to my list
+            self.add(their_item)
+            return True
+        return False
