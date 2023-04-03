d8db175f8782de20a2b3e8c61e5f21e17f5b9ae3
Selene Castillo <secast@live.com>
2023-04-03 13:35:19 -0700

    wave 1 passing all tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, added_item):
+        self.added_item = added_item
+        (self.inventory).append(added_item)
+        return added_item
+    
+    def remove(self,item_removed):
+        self.item_removed = item_removed
+        if item_removed not in self.inventory:
+            return False
+        else:
+            for item in self.inventory:
+                if item_removed == item:
+                    (self.inventory).remove(item)
+                    return item_removed
