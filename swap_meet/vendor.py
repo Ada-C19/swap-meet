b5badd7cffdb7a9ba8ba0b169a18e41ad15580e9
Kat <kmunson13@gmail.com>
2023-04-03 13:51:15 -0400

    Finish wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = self.empty_list_creator(inventory)
+
+    def empty_list_creator(self, inventory):
+        self.inventory = [] if inventory is None else inventory
+        return self.inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        for thing in self.inventory:
+            if item == thing:
+                self.inventory.remove(thing)
+                return item
+        return False
