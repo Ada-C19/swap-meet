7336fff28b8b55aa78436457fb45db1b63c79228
Jay King <jayriverking@gmail.com>
2023-04-03 13:23:32 -0400

    wave 3 done

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -6,3 +6,23 @@ class Item:
         
     def get_category(self):
         return "Item"
+    
+#     In Wave 3 we will write a method to stringify (convert to a string) an Item using str() and write the method swap_items.
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+
+# When we stringify an instance of Item using str(), it returns "An object of type Item with id <id value>.", where <id value> is the id of the Item instance that str() was called on.
+# For example, if we had an Item instance item_a = Item(id=12345), the output of str(item_a) should be "An object of type Item with id 12345.".
+# To accomplish this, you'll want to investigate what calling str() on a class instance does and how you can override such a method. This type of overriding is known as "operator overloading", put simply, it means that the same method exhibits different behavior across instances of different classes. A simple example would be something like + which for strings means "concatenate" but for numbers, means "add", or for lists, means "combine".
+# The remaining tests in wave 3 imply:
+
+# Instances of Vendor have an instance method named swap_items
+# It takes 3 arguments:
+# swap_items takes 3 arguments:
+# an instance of another Vendor (other_vendor), representing the friend that the vendor is swapping with
+# an instance of an Item (my_item), representing the item this Vendor instance plans to give
+# an instance of an Item (their_item), representing the item the friend Vendor plans to give
+# The method removes my_item from this Vendor's inventory, and adds it to the friend's inventory
+# The method removes their_item from the other Vendor's inventory, and adds it to this Vendor's inventory
+# The method returns True
+# If this Vendor's inventory doesn't contain my_item or the friend's inventory doesn't contain their_item, the method returns False
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,3 +22,12 @@ class Vendor:
                 return item
         else:
             return None
+        
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.add(their_item)
+            other_vendor.add(my_item)
+            self.remove(my_item)
+            other_vendor.remove(their_item)
+            return True
+        return False
