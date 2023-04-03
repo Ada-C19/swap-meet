c06db18378c8c00c9e0b046ac61f650021ef99a2
Gabby <gabrielleyoung10@gmail.com>
2023-04-03 16:51:25 -0500

    Add methods to Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,18 @@
 class Vendor:
     
     def __init__(self, inventory=None):
-        inventory = [] if inventory is None else inventory
+        self.inventory = [] if inventory is None else inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+
+        self.inventory.remove(item)
+
+        return item 
+
