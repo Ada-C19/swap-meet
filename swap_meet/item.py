a2dd8a89ed4ac43abfd634a5e8df80948edc152f
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-04-03 13:07:13 -0400

    Test Wave 1

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,9 @@
 class Item:
     pass
+
+# create attribute called 'id': unique integer by default
+# generate numbers with a package called uuid
+# use one of their functions to create large unique numbers meant to be used as identifiers
+# package returns uuid objects not integers but they do have an attribute int that we can assess the value as an integer
+# when initializing an instance of Item, pass in an integer with the keyword argument id to mannually set the items id.
+# each Item has a function called get_category: returns string holding name of class.
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,38 @@
 class Vendor:
-    pass
+
+    # need attribute called inventory: empty list by default
+    # self.iventory
+    # instantiate vendor with keyword argument 'inventory'/pass in list
+    
+    def __init__(self, inventory = None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+
+    # create instance method called add that takes in one item
+    # add that item to inventory via the instance method and return item
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    # create instance method called remove that takes in one item
+    # method removes the matching item from inventory
+    # return item
+    # if no matching item, return false
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+        
+# create instance method called get_by_id
+# takes one argument: an integer that represents an Item's id
+# method returns item with matching id from inventory 
+# if no matching item in inventory, return None.
+
+
+# Instances of Vendor have an instance method named get_by_id
+
