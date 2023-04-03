dfab09517187bc56b4c5acdc39a238a35f076f1c
Julie Loh <jlohusky@gmail.com>
2023-04-03 15:35:01 -0700

    created Item class with id attribute and get_category function

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,10 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        if not id:
+            id = int(uuid.uuid4())
+        self.id = id
+
+    def get_category(self):
+        return self.__class__.__name__
