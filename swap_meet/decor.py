041840e233698dc18c3f0302ec2bfcfb7f673c0e
Sage Choi <coolcuds@gmail.com>
2023-04-04 14:23:05 -0700

    Modified class to inherit from Item and added import.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,18 +1,13 @@
 import uuid
+from swap_meet.item import Item
 
-class Decor:
+class Decor(Item):
     def __init__(self, id=None, width=0, length=0, condition=0):
-        if not id:
-            id = uuid.uuid4().int
-        self.id = id
+        super().__init__(id, condition)
         self.width = width
         self.length = length
-        self.condition = condition
-
-    def get_category(self):
-        return self.__class__.__name__
     
     def __str__(self):
-        return (f"An object of type Decor with id {self.id}. "
+        return (f"{super().__str__()} "
                 f"It takes up a {self.width} by {self.length} sized space.")
     
