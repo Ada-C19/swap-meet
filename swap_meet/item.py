5230a1d44fcc2c911e6eca2918ffd399d5dcfd6d
Sage Choi <coolcuds@gmail.com>
2023-04-04 14:12:02 -0700

    Added condition_description() method to Item class that returns a description based on the condition.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,13 +1,28 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         if not id:
             id = uuid.uuid4().int
         self.id = id
+        self.condition = condition
 
     def get_category(self):
         return self.__class__.__name__
     
     def __str__(self):
-        return f"An object of type Item with id {self.id}."
+        return f"An object of type {self.get_category()} with id {self.id}."
+    
+    def condition_description(self):
+        if round(self.condition) == 1.0:
+            return "Are you sure you really want this...?"
+        elif round(self.condition) == 2.0:
+            return "Fair condition, but will require fixing up."
+        elif round(self.condition) == 3.0:
+            return "Good condition."
+        elif round(self.condition) == 4.0:
+            return "Great condition! Gently used for sure."
+        elif round(self.condition) == 5.0:
+            return "Mint condition, item is in excellent shape!"
+        else:
+            return "Not sure how to grade this item...(º～º)"
