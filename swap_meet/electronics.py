7f84333af17751f63bad8d996825f2d77b9d8895
Julie Loh <jlohusky@gmail.com>
2023-04-04 11:56:33 -0700

    created Electronics child class, parent class Item

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,13 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, id=None, type=None):
+        super().__init__(id)
+
+        if not type:
+                type = "Unknown"
+        self.type = type
+    
+    def __str__(self):
+        return (super().__str__() + ' This is a ' + 
+                str(self.type) + ' device.')
