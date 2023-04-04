d5586e27e982d9c4e8cb64f9399fb08709dd3081
Divya <divya.naidoo5@gmail.com>
2023-04-04 10:33:09 -0700

    changed wave 1 to have default value as None instead of an empty list, tests stil pass

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,12 +1,14 @@
 class Vendor:
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=None):
+        if inventory is None:
+            inventory = []
         self.inventory = inventory
     
-    def add(self, item):
+    def add_item(self, item):
         self.inventory.append(item)
         return item
     
-    def remove(self, item):
+    def remove_item(self, item):
         if not item in self.inventory:
             return False
         else:
