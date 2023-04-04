8d2a139806df0939d9c95cdf38a52956ae9b07ba
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-04-03 17:57:39 -0700

    implemented functionality for swap_items in Vendor and wrote assertion statements for incomplete test; all tests pass

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -23,3 +23,22 @@ class Vendor:
             if item.id == item_id:
                 return item
         return None
+    
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+
+        # verify each item is in their corresponding inventory
+        # else return False
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        
+        # remove my_item from our own inventory and add to the other vendor's inventory
+        self.remove(my_item)
+        other_vendor.add(my_item)
+
+        # remove their_item from other vendor's inventory and add to ours
+        self.add(their_item)
+        other_vendor.remove(their_item)
+
+        return True
+
