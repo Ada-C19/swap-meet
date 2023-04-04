1bc9f1433993111d1174fd4c1444bbfb2678ed61
Gabby <gabrielleyoung10@gmail.com>
2023-04-04 15:12:44 -0500

    Add __str__ to Item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,15 @@
 import uuid
 
+
 class Item:
-    def __init__(self, id= None):
+    def __init__(self, id=None):
         if id is None:
             self.id = uuid.uuid4().int
         else:
             self.id = id
-    
+  
     def get_category(self):
-        return self.__class__.__name__ 
+        return self.__class__.__name__
+
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
