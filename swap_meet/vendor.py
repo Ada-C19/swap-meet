5081c3f23da85fee0eb528a43b0a2ed7ea89fe3c
Amber Shay <mashaad21@gmail.com>
2023-04-04 09:17:28 -0600

    Wave 4 complete; tests passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -28,17 +28,29 @@ class Vendor:
                 return item
         return None
 
-    def swap_items(self, vendor, my_item, their_item):
+    def swap_items(self, other_vendor, my_item, their_item):
        """ Removes items from both vendor's inventories and swaps them. If item from one vendor is not present, no items will be swapped. """
        #Remove items from both inventories
-       other_remove = vendor.remove(their_item)
+       other_remove = other_vendor.remove(their_item)
        if not other_remove:
           return False
        self_remove = self.remove(my_item)
        if not self_remove:
-          vendor.add(their_item)
+          other_vendor.add(their_item)
           return False
        #Add items to both inventories
        self.add(their_item)
-       vendor.add(my_item)
+       other_vendor.add(my_item)
+       return True
+
+    def swap_first_item(self, other_vendor):
+       """ Swaps the first item in each vendor's inventory. """
+       if not (self.inventory and other_vendor.inventory):
+          return False
+
+       self_first = self.inventory[0]
+
+       self.inventory[0] = other_vendor.inventory[0]
+       other_vendor.inventory[0] = self_first
+
        return True
