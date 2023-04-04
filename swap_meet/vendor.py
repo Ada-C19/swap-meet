47d131eb053d2f1558bdd7fd80b595bff62c7c65
Valerie Valentin <valerie.valentine93@gmail.com>
2023-04-03 22:14:25 -0700

    Finished Wave 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,21 @@
 class Vendor:
-    # attribute -> inventory == []
     def __init__(self, inventory = None):
         self.inventory = [] if inventory is None else inventory
-    # method -> takes in item, adds item to inventoryattribute, returns item 
+
     def add(self, item): 
         self.inventory.append(item)
         return item
-    # method -> takes in item, removes the matching item, returns item
-    #        -> if no match, return False
+ 
     def remove(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
             return item
         return False
+    
+    def get_by_id(self, item_id):
+        for item in self.inventory:
+            if item.id == item_id:
+                return item
+        return None
+
+
