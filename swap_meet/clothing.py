6ea503d0199d7b4bc4b1f7dee5a6a4ba74f687d4
Dee Williams <deejmwilliams@gmail.com>
2023-04-04 13:44:24 -0400

    Remove scratch comments from modules

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -10,14 +10,3 @@ class Clothing(Item):
     def __str__(self):
         """ Override """
         return f"{super().__str__()} It is made from {self.fabric} fabric."
-
-
-# Clothing`
-#   - Has an attribute `id` that is by default a unique integer
-#   - Has an attribute `fabric` that is by default the string "Unknown"
-#     - This attribute describes what fabric the clothing is made from; some example values might be `"Striped"`, `"Cotton"`, or `"Floral"`
-#     - When we instantiate an instance of `Clothing`, we can optionally pass in a string with the keyword argument `fabric`
-#   - Has a function `get_category` that returns `"Clothing"`
-#   - Has a stringify method that returns `"An object of type Clothing with id <id value>. It is made from <fabric value> fabric."`
-#     - For example, if we had a `Clothing` instance with an `id` of `123435` and a `fabric` attribute that holds `"Wool"`, 
-# its stringify method should return `"An object of type Clothing with id 12345. It is made from Wool fabric."`
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -9,15 +9,3 @@ class Electronics(Item):
 
     def __str__(self):
         return f"{super().__str__()} This is a {self.type} device."
-
-
-
-#  `Electronics`
-#   - Has an attribute `id` that is by default a unique integer
-#   - Has an attribute `type` that is by default the string "Unknown"
-#     - This attribute describes what kind of electronic device this is. Some example values might be `“Kitchen Appliance”`, `“Game Console”`, or `“Health Tracker”`
-#     - When we initialize an instance of `Electronics`, we can optionally pass in a string with the keyword argument `type`
-#   - Has an function `get_category` that returns `"Electronics"`
-#   - Has a stringify method that returns `"An object of type Electronics with id <id value>. This is a <type value> device."`
-#     - For example, if we had an `Electronics` instance with an `id` of `123435` and `type` attribute of `"Mobile Phone"`, its stringify method should return 
-# `"An object of type Electronics with id 12345. This is a Mobile Phone device."`
