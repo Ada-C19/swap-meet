d48747f631896f412e0aa15af927ce0cef9fd777
Sage Choi <coolcuds@gmail.com>
2023-04-04 11:46:31 -0700

    Added constructor and instance methods to Decor class.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,17 @@
+import uuid
+
 class Decor:
-    pass
+    def __init__(self, id=None, width=0, length=0):
+        if not id:
+            id = uuid.uuid4().int
+        self.id = id
+        self.width = width
+        self.length = length
+
+    def get_category(self):
+        return self.__class__.__name__
+    
+    def __str__(self):
+        return (f"An object of type Decor with id {self.id}. "
+                f"It takes up a {self.width} by {self.length} sized space.")
+    
