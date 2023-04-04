2a9736a68bcaf0d72a2db82ad1a8acb02b18d0b3
Carolyn Qi <qi.carolyn@gmail.com>
2023-04-04 14:17:46 -0700

    Add condition and condition_description to item and all child classes

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,13 +1,16 @@
 import uuid
 class Item:
-    def __init__(self, id = None):
+    def __init__(self, id = None, condition = 0):
         if not id:
             id = uuid.uuid4().int
         self.id = id
-        print(self.id)
+        self.condition = condition
 
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}."
 
     def get_category(self):
         return f"{self.__class__.__name__}"    
+    
+    def condition_description(self):
+        return str(self.condition)
