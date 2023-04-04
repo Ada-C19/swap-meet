62e5b2caa4afc6f258b8dc9c7a10bdeb74817369
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-04-04 18:58:41 -0400

    need to finish wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,5 @@
 import uuid
 
-
 class Item:
     def __init__(self, id = None):
         if id is None:
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,3 @@
-
-
 class Vendor:
     def __init__(self, inventory = None):
         self.inventory = [] if inventory is None else inventory
@@ -19,5 +17,15 @@ class Vendor:
         for item in self.inventory:
             if item.id == id:
                 return item
+            
+    def swap_items(self, other_vendor, my_item, their_item):
+        if their_item not in self.inventory:
+            if my_item not in other_vendor.inventory:
+                self.inventory.remove(my_item)
+                other_vendor.inventory.remove(their_item)
+                self.inventory.append(their_item)
+                other_vendor.inventory.append(my_item)
+                return True
+        return False 
         
         
