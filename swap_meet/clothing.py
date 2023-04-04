ee86e25675a14cd1df494c25ee8ae0cbb24482ff
Jiaping Chen <jiapingchen17@gmail.com>
2023-04-03 22:28:46 -0700

    still failing 5 tests cases for wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,20 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    
+    def __init__(self,id, fabric = "Unknown", condition = 0):
+        super().__init__(condition)
+        self.id = id
+        self.fabric = fabric
+        self.condition = condition
+        
+
+    def get_category(self):
+        return "Clothing"
+    
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+    
+
+     # not passing the first test case for Clothing
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,8 @@
 import uuid
 class Item:
-    def __init__(self, id = None):
+    def __init__(self, id, condition = 0):
+        self.condition = condition
+       
         if id is None:
             self.id = uuid.uuid4().int
         else:
@@ -14,3 +16,17 @@ class Item:
     def __str__(self):
         return f"An object of type Item with id {self.id}."
 
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "Brand new with tags"
+        elif self.condition == 1 or self.condition < 2 :
+            return "Like new"
+        elif self.condition == 2 or self.condition < 3 :
+            return "Good"
+        elif self.condition == 3 or self.condition < 4:
+            return "Fair"
+        elif self.condition == 4 or self.condition < 5:
+            return "Heavily used"
+        else:
+            return "Poor"
