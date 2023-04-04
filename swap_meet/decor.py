b159d39a673a8fe53434783bb251581142472c41
Sage Choi <coolcuds@gmail.com>
2023-04-04 13:23:48 -0700

    Removed extra blank line.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,12 +1,13 @@
 import uuid
 
 class Decor:
-    def __init__(self, id=None, width=0, length=0):
+    def __init__(self, id=None, width=0, length=0, condition=0):
         if not id:
             id = uuid.uuid4().int
         self.id = id
         self.width = width
         self.length = length
+        self.condition = condition
 
     def get_category(self):
         return self.__class__.__name__
