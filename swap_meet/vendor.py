e04f70b97721fbd5952939a0862beea9e5ed42a8
Yael Peet <yaelpeet@gmail.com>
2023-04-03 22:28:26 -0400

    halfway point wave 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -15,3 +15,9 @@ class Vendor:
             return item
         else:
             return False
+
+    def get_by_id(self, id):
+# 7. Inside the Vendor class, define a method: get_by_id that takes one argument: an integer representing an Item's id.
+# 8. Inside the get_by_id method, search the Vendor's inventory for an item with a matching id.
+# 9. If there is a matching item, return that item.
+# 10. If there is no matching item, return None
