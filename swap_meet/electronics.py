ad7d54edbc7c23663058871efed04f9edadb4a20
Sage Choi <coolcuds@gmail.com>
2023-04-04 14:23:34 -0700

    Modified class to inherit from Item and added import.

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,17 +1,12 @@
 import uuid
+from swap_meet.item import Item
 
-class Electronics:
+class Electronics(Item):
     def __init__(self, id=None, type="Unknown", condition=0):
-        if not id:
-            id = uuid.uuid4().int
-        self.id = id
+        super().__init__(id, condition)
         self.type = type
-        self.condition = condition
-
-    def get_category(self):
-        return self.__class__.__name__
     
     def __str__(self):
-        return (f"An object of type Electronics with id {self.id}. "
+        return (f"{super().__str__()} "
                 f"This is a {self.type} device.")
     
