88be6b1fe277bfed801584a8b7804125f4cfb974
Sophia Tran <sophiat8832@gmail.com>
2023-04-04 15:44:50 -0700

    imported Item class and implemented constructor for Clothing class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,6 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, id=None, fabric="Unknown"):
+        super().__init__()
+        self.fabric = fabric
