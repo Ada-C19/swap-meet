f49d29b70222861f833aaa41bf4f2ee7a8700f51
Lyuda Kim <lyudarkim@gmail.com>
2023-04-03 20:29:20 -0700

    Wrote Clothing class, passing 3/4 tests.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,13 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+
+class Clothing(Item):
+    
+    def __init__(self, id, fabric="Unknown"):
+        super().__init__(id)
+        self.id = id
+        self.fabric = fabric
+
+    def __str__(self):
+        category_id_description = super().__str__()
+        return f"{category_id_description} It is made from {self.fabric} fabric."
