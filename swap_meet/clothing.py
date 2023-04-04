0ab219202646ad51587a571b589b5259d94ceb16
Agnes Kim <agneskim4@gmail.com>
2023-04-04 14:33:43 -0400

    updated clothing Class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,13 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, fabric = "Unknown"):
+        super().__init__(id= None)
+        self.fabric = fabric
+    
+    # def get_category(self):
+    #     super().get_category()
+    
+    def __str__(self):
+        super().__str__()
+        return f"It is made from {self.fabric} fabric."
