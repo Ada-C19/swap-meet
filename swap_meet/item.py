7d602b400e22b5b8a39c71b0b2678bc057bbf3d8
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-04-03 18:19:29 -0600

    Updated code--still issues with pytest

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,29 @@
 import uuid
 class Item:
     
-    def __init__(self, id): 
-        self.self = self
-        self.id =
+    def __init__(self, id=None): 
+        # self.self = self
+        
+        if id is None:
+            # Generate a random UUID
+            random_uuid = uuid.uuid4()
+            # Convert the UUID to an integer using its hex representation
+            random_id = int(random_uuid.hex, 16)
+            #Assign the integer id to self
+            self.id = random_id
+        else:
+            self.id = id
+    
+    def get_category(self):
+        return f"{self.category}"
+    
+    #Use dunder str to stringify the id
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+
+    
+
+
+
+
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -26,3 +26,28 @@ class Vendor:
             if item.id == id:
                 return item
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.inventory.remove(my_item)
+            other_vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            other_vendor.inventory.append(my_item)
+            return True
+        else:
+            return False
+        
+    def swap_first_item(self, other_vendor):
+        
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        my_item = self.inventory[0]
+        their_item = other_vendor.inventory.pop(0)
+        self.inventory.append(their_item)
+        other_vendor.inventory.append(my_item)
+        return True
+    
+    
+        
+        
