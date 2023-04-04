4c54b280b0f5053df8391b0aceeeb6004ab47761
Sophia Tran <sophiat8832@gmail.com>
2023-04-04 15:50:16 -0700

    implemented get_category in Clothing class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -4,3 +4,6 @@ class Clothing(Item):
     def __init__(self, id=None, fabric="Unknown"):
         super().__init__()
         self.fabric = fabric
+
+    def get_category(self):
+        return "Clothing"
