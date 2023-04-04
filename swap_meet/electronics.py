ca150f5d608726d2f93a8015a1fff9e4ed89de57
Amanda <jonesamanda996@gmail.com>
2023-04-04 14:44:24 -0400

    Decor, Electronics and added condition attribute

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,14 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+class Electronics(Item):
+    def __init__(self, id=None, condition=0, type="Unknown"):
+        super().__init__(id, condition)
+        self.type = type
+
+    def get_category(self):
+        return super().get_category()
+    
+    def __str__(self):
+        return f"{super().__str__()} This is a {self.type} device."
+
+    def condition_description(self):
+        return super().condition_description()
