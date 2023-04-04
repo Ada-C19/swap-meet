950ecd2d9fc9841bf3f3d022cc5fe7c8437b083f
Elizabeth Valle <e7valle@gmail.com>
2023-04-04 15:38:51 -0700

    Passed Wave 3 Tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,3 +22,18 @@ class Vendor:
             if item.id == item_id:
                 return item
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        
+        # remove my_item from this Vendor's inv
+        self.remove(my_item)
+        # add this to friends inv
+        other_vendor.add(my_item)
+        # remove their_item from the other Vendors inv
+        other_vendor.remove(their_item)
+        # add this to vendor's inv
+        self.add(their_item)
+
+        return True
