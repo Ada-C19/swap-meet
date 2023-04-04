c1466eecf8b8947450be06a1aaf7c1039ba70638
jasmine <jtmsoto@gmail.com>
2023-04-04 14:56:13 -0700

    Add Electronics child class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,15 @@
-class Electronics:
-    pass
+from uuid import uuid4
+from swap_meet.item import Item
+
+
+class Electronics(Item):
+    def __init__(self, id=None, type="Unknown"):
+        super().__init__(id)
+        self.type = type
+
+    def get_category(self):
+        return "Electronics"
+    
+    def __str__(self):
+        return (f"{super().__str__()} "
+                f"This is a {self.type} device.")
