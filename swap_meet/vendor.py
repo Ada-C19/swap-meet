78d15cbc1841d90fb29cac6476707763eada2069
Ericka <erickamoreno529@gmail.com>
2023-04-04 13:50:40 -0700

    finished wave_1 and updated wave_1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+
+    def add(self, item):
+        if item not in self.inventory:
+            self.inventory.append(item)
+            return item
+        else:
+            return False
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False 
+    
