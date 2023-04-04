fc3c42ad3235151e40786f44b06d64d962e74ada
Lily <aulily09@gmail.com>
2023-04-03 17:29:19 -0700

    functional get_by_id(), passed 6/6 tests for wave_2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,4 @@
+from .item import Item
 class Vendor:
     
     def __init__(self, inventory=[]):
@@ -12,3 +13,9 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
