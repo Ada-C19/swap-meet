611e17173276a87a85cc290ffb8e5824b1aa4d27
Limary Gonzalez <limaryg11@gmail.com>
2023-04-04 11:43:41 -0700

    wave 3 completed, tests pasts- need to go back and review

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -6,10 +6,14 @@ class Item:
         # passing in large random int if no id being passed by user
         # allowing id to be overwritten if id being passed by user
         if id is None:
-            self.id = uuid.uuid4().int
-        else:
-            self.id = id
+            id = uuid.uuid4().int
+        self.id = id
     
     def get_category(self):
         # returns any objects name
         return self.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}."
+
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,9 +1,11 @@
+from swap_meet.item import Item
 
 class Vendor:
-    def __init__(self, inventory = []):
+    def __init__(self, inventory = None): 
+        if inventory is None:
+            inventory = []
         self.inventory = inventory
         self.name = "Vendor"
-    
     
     def add(self, item):
         self.inventory.append(item)
@@ -16,8 +18,22 @@ class Vendor:
         self.inventory.remove(item)
         return item
 
-    def get_by_id(self, item_id):
+    def get_by_id(self, current_id):
+        # item_id = Item().id
         for item in self.inventory:
-            if item == item_id:
+            if current_id == item.id:
                 return item
         return None
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        other_vendor_inventory = other_vendor.inventory
+        current_vendor_inventory = self.inventory
+        if my_item not in current_vendor_inventory or their_item not in other_vendor_inventory:
+            return False
+        
+        current_vendor_inventory.remove(my_item)
+        other_vendor_inventory.append(my_item)
+        other_vendor_inventory.remove(their_item)
+        current_vendor_inventory.append(their_item)
+        return True
+
