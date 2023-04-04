349589e1b1a94889e59e981d9e9156ee0701f525
Gloria <gloria.wan.glory@gmail.com>
2023-04-04 15:26:12 -0400

    working on wave 5 parent class/inheritance

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,9 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, id, name, fabric = None):
+        super().__init__(id, name = "Clothing")
+        
+        self.fabric = fabric if fabric else "Unknown"
+    
+    
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,9 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None):
-        self.id = id if id != None else uuid.uuid4().int
-        self.name = "Item"
+    def __init__(self, id=None, name=None):
+        self.id = id if id else uuid.uuid4().int
+        self.name = name if name else "Item"
 
     def get_category(self):
         return self.name
