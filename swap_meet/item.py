4ff6bdad3ada33494eb0e604fb34f02772544029
Angelica <angelica.yee57@gmail.com>
2023-04-04 15:21:18 -0500

    w1 and w2 except get_by_id function not working properly

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
+import uuid
+from uuid import uuid4
 class Item:
-    pass
+    def __init__(self, id = None):
+        id = uuid.uuid4().int if id is None else id
+        self.id = id
+
+    def get_category(self):
+        return self.__class__.__name__
+    
+prueba = Item()
+prueba2 = Item()
+print(type(prueba.id),prueba.id)
+print(type(prueba2.id),prueba2.id)
+#uuid.uuid4().int
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,25 @@
+
 class Vendor:
-    pass
+    
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else: 
+            return False
+    
+    def add(self, x):
+        self.inventory.append(x)
+        return x
+    
+    def get_by_id(self, element):
+        if element in self.inventory:
+            return element
+        else: 
+            return None
 
 
 
