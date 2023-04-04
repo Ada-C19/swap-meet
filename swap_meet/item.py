c61f0e8e5fa2b1b67f35c647484d3f2c3a5ef93a
Zhuo <zhuo.parse@gmail.com>
2023-04-04 10:41:02 -0400

    Add functions and test to accomplish wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -4,5 +4,9 @@ class Item:
     def __init__(self, id=None):
         self.id = int(uuid.uuid4()) if id is None else id
 
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+
     def get_category(self):
         return f"{self.__class__.__name__}"
+    
