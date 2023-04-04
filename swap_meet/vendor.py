33806c522663d8ad8a1aba61bf86d1a4031f38dd
Selene Castillo <secast@live.com>
2023-04-03 17:26:49 -0700

    started wave 3 geeerrrl

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,7 +18,21 @@ class Vendor:
                     return item_removed
 
 # not sure if code below belongs in the Item class
+# passed all tests, so I guess iz ok
     def get_by_id(self, id_num):
         for item in self.inventory:
             if item.id == id_num:
                 return item
+            
+    def swap_items(self, other_vendor, my_item, their_item):
+        self.other_vendor = other_vendor
+        self.my_item = my_item
+        self.their_item = their_item
+        if my_item not in other_vendor.inventory or their_item not in self.inventory:
+            return False
+        else:
+            self.add(their_item)
+            other_vendor.add(my_item)
+            self.remove(my_item)
+            other_vendor.remove(their_item)
+            return True
