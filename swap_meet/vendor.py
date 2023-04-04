b0768226fa0e6f3d37e145bd295f9557201b6533
Selene Castillo <secast@live.com>
2023-04-04 14:06:32 -0700

    wave 4 complete, hang 10

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -3,12 +3,10 @@ class Vendor:
         self.inventory = inventory
 
     def add(self, added_item):
-        # self.added_item = added_item
         (self.inventory).append(added_item)
         return added_item
     
     def remove(self,item_removed):
-        # self.item_removed = item_removed
         if item_removed not in self.inventory:
             return False
         else:
@@ -17,24 +15,24 @@ class Vendor:
                     (self.inventory).remove(item)
                     return item_removed
 
-# not sure if code below belongs in the Item class
-# passed all tests, so I guess iz ok
     def get_by_id(self, id_num):
         for item in self.inventory:
             if item.id == id_num:
                 return item
             
     def swap_items(self, other_vendor, my_item, their_item):
-        # self.other_vendor = other_vendor
-        # self.my_item = my_item
-        # self.their_item = their_item
-        # if my_item not in other_vendor.inventory or their_item not in self.inventory:
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
-
             return False
         else:
             self.add(their_item)
             other_vendor.add(my_item)
             self.remove(my_item)
             other_vendor.remove(their_item)
+            return True
+        
+    def swap_first_item(self, other_vendor):
+        if self.inventory == [] or other_vendor.inventory == []:
+            return False
+        else:
+            self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
             return True
