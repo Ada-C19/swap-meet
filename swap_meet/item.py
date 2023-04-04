a509f8d90760fc7bde3eb236b6ec7a2217b82e3a
Laura <lauraemiliaperez@outlook.com>
2023-04-03 17:16:53 -0700

    part of wave 2 and part of wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,16 @@
+import uuid
 class Item:
-    pass
+    def __init__(self, id=None):
+        if id is None:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+
+    def __str__(self):
+        my_string = f'{self.id}'
+        return f"An object of type {self.get_category()} with id {my_string}."
+    
+    def get_category(self):
+        return self.__class__.__name__
+    
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,10 @@
+from swap_meet.item import Item
 class Vendor:
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=None):
+        if inventory is None:
+           inventory = []
         self.inventory = inventory
+
     
     def add(self, item):
         self.inventory.append(item)
@@ -11,3 +15,25 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+    def get_by_id(self, id):
+        # returns the item with matching id from inventory
+        for element in self.inventory:
+            if element.id == id:
+                return element
+            return None
+    def swap_items(self, other_vendor, my_item, their_item):
+        """The method removes my_item from this Vendor's inventory, 
+        and adds it to the friend's inventory
+        The method removes their_item from the other Vendor's inventory, 
+        and adds it to this Vendor's inventory"""
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        else:
+            self.inventory.remove(my_item)
+            other_vendor.inventory.append(my_item)
+            other_vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            return True    
+        
+        
+    
