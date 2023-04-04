4c75cdecdb3c9a0901fbb4b142f9b0dc6189d82b
Lily <aulily09@gmail.com>
2023-04-03 20:15:02 -0700

    functional __str__()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -8,3 +8,8 @@ class Item:
 
     def get_category(self):
         return str(self.__class__.__name__)
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+    
+        
