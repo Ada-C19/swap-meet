8a83fb9f2dcaf877516bcff7cc167a899a377575
Melinda Mott <melinda.mott@gmail.com>
2023-04-04 14:27:09 -0700

    Stringify

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -6,6 +6,9 @@ class Item:
         else:
             self.id = id 
 
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+
     def get_category(self):
         return "Item"
     
