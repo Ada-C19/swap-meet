c5480b4537f59ac79b3489547fc8e68ff9fe835a
Lily <aulily09@gmail.com>
2023-04-03 21:34:53 -0700

    Functional swap_first_item(), refractored code by creating helper function, made it more readable

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,15 +18,30 @@ class Vendor:
         for item in self.inventory:
             if item.id == id:
                 return item
+            
         return None
     
-    def swap_items(self, other_vendor, my_item, their_item):
-        if my_item not in self.inventory or their_item not in other_vendor.inventory:
-            return False
-        
+    def swap_items_helper(self, other_vendor, my_item, their_item):
         self.inventory.remove(my_item)
         other_vendor.inventory.append(my_item)
         other_vendor.inventory.remove(their_item)
         self.inventory.append(their_item)
-        return True
 
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        
+        self.swap_items_helper(other_vendor, my_item, their_item)
+        return True
+    
+    def swap_first_item(self, other_vendor):
+        if len(self.inventory) == 0 or len(other_vendor.inventory) == 0:
+            return False
+        
+        my_item = self.inventory[0]
+        their_item = other_vendor.inventory[0]
+
+        self.swap_items(other_vendor, my_item, their_item)
+
+        return True
+        
