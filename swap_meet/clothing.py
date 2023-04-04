ff7378fa13d9bf45242ed0c42b59b57fc8ceff12
Lily <aulily09@gmail.com>
2023-04-03 21:57:44 -0700

    Passing all but one test

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,14 @@
-class Clothing:
-    pass
+from .item import Item
+
+class Clothing(Item):
+
+    def __init__(self, id, fabric="Unknown"):
+        super().__init__(id)
+        self.fabric = fabric
+
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+    
+    def get_category(self):
+        return "Clothing"
+    
