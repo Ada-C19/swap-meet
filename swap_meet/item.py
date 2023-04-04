2473a0a8c5d4eb7180517f0692483013bfa8a2c7
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2023-04-04 17:51:06 -0300

    adding Wave 1 and 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,13 @@
+from uuid import uuid4
+
 class Item:
-    pass
+
+    def __init__(self, id=0):
+        self.id = id if id else uuid4().int
+        
+    def get_category(self):
+        return self.__class__.__name__
+    
+
+a = Item()
+print(a.get_category())
