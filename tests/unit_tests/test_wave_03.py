7b4c36bb6e547b04977e004b8922a373a98f72af
Jamie <jamie.danielle@denverrollerderby.org>
2023-04-04 16:58:08 -0600

    more assertions in wave 3 tests

--- tests/unit_tests/test_wave_03.py
+++ tests/unit_tests/test_wave_03.py
@@ -132,6 +132,8 @@ def test_swap_items_from_their_empty_returns_false():
     result = fatimah.swap_items(jolie, item_b, nobodys_item)
 
     assert not result 
+    assert len(jolie.inventory) == 0
+    assert len(fatimah.inventory) == 3
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
