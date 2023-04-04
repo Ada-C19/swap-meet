09c740aeb422e271ef1ccff79727975db0fdc0ba
April Zhang <aprilzh23@gmail.com>
2023-04-04 00:57:59 -0700

    finished wave 1&2 draft

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,12 @@
+import uuid
 class Item:
-    pass
+    
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            self.id = uuid.uuid4.int
+    
+    def get_category(self):    
+        return self.__class__.__name__
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,21 @@
+from item import Item
 class Vendor:
-    pass
+    def __init__(self):
+        self.inventory = []
+        
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if id == self.id:
+                return item
+        return None
