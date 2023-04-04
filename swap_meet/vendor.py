5a815f7ce99466439830e99ca1b2c745c40501e0
Angela <echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"')  /Users/an5073/.zprofile>
2023-04-04 08:22:02 -0400

    Angela's additions to Wave 3 before merge

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,4 +18,21 @@ class Vendor:
         for item in self.inventory:
             if item.id == id:
                 return item
-        return None 
+        return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        #from vendor inventory remove my_item and add to other_vendor inventory
+        #from other_vendor inventory remove their_item and add to vendor inventory
+        #return True
+        #if either vendor doesn't have item in inventory return False
+        if my_item in self.inventory:
+            self.inventory.remove(my_item)
+            other_vendor.inventory.append(my_item)
+            return True
+        elif their_item in other_vendor.inventory:
+            other_vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            return True
+        else:
+            return False
+        
