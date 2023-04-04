7093bb18f0476ab5ff9388f3ba4ac8ac1dd2e946
Yvett <yvett.nez@gmail.com>
2023-04-03 21:07:47 -0500

    wave 2 complete

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,10 +1,8 @@
+from .item import Item
+
 class Vendor:
-    def __init__(self, inventory = []):
-        # if inventory is None:
-        #     self.inventory = []
-        # else:
-        #     self.inventory = inventory
-        self.inventory = inventory
+    def __init__(self, inventory = None):
+        self.inventory = [] if inventory is None else inventory
 
 
     def add(self, item):
@@ -16,3 +14,9 @@ class Vendor:
             return False
         self.inventory.remove(item)
         return item
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
