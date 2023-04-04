6aac521f314cd1ddadcf29f9f683d3d4099c044a
Jennifer Tam <jenniferwtam@gmail.com>
2023-04-03 21:05:05 -0700

    Wave 5 Passed All Tests

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,8 +1,8 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self, id = None, type = "Unknown"):
-        super().__init__(id)
+    def __init__(self, id = None, type = "Unknown", condition = 0):
+        super().__init__(id, condition)
         self.type = type
 
     def get_category(self):
@@ -10,3 +10,6 @@ class Electronics(Item):
     
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
+
+    def condition_description(self):
+        return super().condition_description()
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,12 @@
 import uuid
 
 class Item:
-    def __init__(self, id = None):
+    def __init__(self, id = None, condition = 0):
         '''
         If id does not exist, assign unique id with uuid
         Otherwise, self.id = id
         '''
+        self.condition = condition
         if not id:
             self.id = int(uuid.uuid4())
         else:
@@ -17,3 +18,19 @@ class Item:
     
     def __str__(self):
         return "An object of type Item with id {}.".format(self.id)
+        
+    def condition_description(self):
+        if self.condition == 0:
+            return "That's Gross"
+        elif self.condition == 1:
+            return "No Thanks"
+        elif self.condition == 2:
+            return "Not Bad"
+        elif self.condition == 3:
+            return "Passable"
+        elif self.condition == 4:
+            return "Pretty Nice"
+        elif self.condition == 5:
+            return "I Need This!"
+        else:
+            return "Unknown"
