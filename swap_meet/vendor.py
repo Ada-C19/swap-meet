ccbc90d812f29c7ca88a7018d493cf611537a9e3
Angela <echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"')  /Users/an5073/.zprofile>
2023-04-04 12:31:26 -0400

    Wave 3 done: defined swap_first_item

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -32,20 +32,20 @@ class Vendor:
         other_vendor.add(my_item)
         return True
     
-    # def swap_items(self, other_vendor, my_item, their_item):
-    #     #from vendor inventory remove my_item and add to other_vendor inventory
-    #     #if either vendor doesn't have item in inventory return False
-    #     if my_item in self.inventory:
-    #         self.inventory.remove(my_item)
-    #         other_vendor.inventory.append(my_item)
-    #         return True
-    #     #from other_vendor inventory remove their_item and add to vendor inventory
-    #     elif their_item in other_vendor.inventory:
-    #         other_vendor.inventory.remove(their_item)
-    #         self.inventory.append(their_item)
-    #         return True
-    #     else:
-    #         return False
+
+
+    def swap_first_item(self, other_vendor):
+        #remove first item from self.inventory and add to other_vendor.inventory
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        self.inventory[0], other_vendor.inventory[0] = other_vendor.inventory[0], self.inventory[0]
+        return True
+        
+        
+        
+
+
+    
         
 
             
