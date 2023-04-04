82a1682d0b7244266eecdc1e754990ea717ff5fd
dreinicke27 <danicareinicke@gmail.com>
2023-04-04 10:35:21 -0700

    Added error handling to item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,18 +1,18 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None, condition=None, age=None):
-        #if type(id) != int:
-            #raise TypeError("id must be an int")
+    def __init__(self, id=None, condition=0, age=0):
+        if type(id) != int and id != None:
+            raise TypeError("id must be an int")
         if not id:
             id = uuid.uuid4().int
         self.id = id
-        if not condition:
-            condition = 0
+        if type(condition) != int and type(condition) != float and condition != None:
+            raise TypeError("condition must be an int or a float")
         self.condition = condition
-        if not age:
-            age = 0
-        self.age = age
+        if type(age) != int and type(age) != float and age != None:
+            raise TypeError("age must be an int or a float")
+        self.age = age 
     
     def get_category(self):
         return self.__class__.__name__
