179599fbbc55677d58eeb617a1adfe6286666bcc
Madison Jackson <madi.jackson29@gmail.com>
2023-04-04 15:40:53 -0700

    accept merge changes

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,7 +5,6 @@ class Item:
         self.id = uuid.uuid4().int if id is None else id
         
     def get_category(self):
-        # return "Item"
         return (f"{self.__class__.__name__}")
     
     def get_by_id(self):
