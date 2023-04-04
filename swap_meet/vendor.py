024c0ae052b7fd9c65bc8a85119d5350c1e39733
Mikayla Howden <MDHowden2020@gmail.com>
2023-04-03 21:19:31 -0700

    Completed wave 1 and wave 1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,22 @@
 class Vendor:
+
+    def __init__(self, inventory=None): # inventory = empty list. Optionally pass in Keyword argument inventory?
+        if inventory is None: 
+            inventory = []
+        self.inventory = inventory
+
     
-# Starting here 
+    def add(self, item):
+        #  adds the item to the inventory
+        self.inventory.append(item)
+        # returns the item that was added
+        return item
+    
+    def remove(self, item):
+        # method removes the matching item from the inventory
+        if item in self.inventory: 
+            self.inventory.remove(item)
+        # returns the item that was removed
+            return item 
+        #If there is no matching item in the inventory, the method should return False
+        return False 
