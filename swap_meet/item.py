aae142ac564416676da73b2104a2b05304ac9319
Nadia Mirzai <nadia.m.mirzai@gmail.com>
2023-04-04 16:22:53 -0400

    one test needs to be fixed in wave_2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,22 @@
+import uuid
+# from swap_meet.vendor import Vendor
+
 class Item:
-    pass
+    def __init__(self, id = None):
+        if id != None:
+            self.id = id
+        else:
+            self.id = uuid.uuid4().int
+
+    
+    def get_category(self):
+        class_name = "Item"
+        # class_name = __class__.__name__
+        return class_name
+
+        
+    
+    
+
+
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,6 @@
+from swap_meet.item import Item
+import uuid
+
 class Vendor:
     def __init__(self, inventory = None):
         self.inventory = [] if inventory is None else inventory 
@@ -12,5 +15,13 @@ class Vendor:
         
         self.inventory.remove(item)
         return item
+    
+    def get_by_id(self, id):
+        if id not in self.inventory:
+            return None
+        self.id = Item(id)
+    
+  
+
 
         
