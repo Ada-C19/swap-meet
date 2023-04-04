c4095d629380b1d40ea2878a4715a12fb66eda4e
Amanda <jonesamanda996@gmail.com>
2023-04-04 12:54:40 -0400

    clothing changes

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,6 +2,6 @@ from swap_meet.item import Item
 class Clothing(Item):
 	def __init__(self, id, fabric=“Unknown”):
 		super().__init__(id)
-        self.fabric = fabric if fabric is None else fabric
+        self.fabric = fabric if fabric is fabric else "Unknown"
 	
     def get_category(self):
