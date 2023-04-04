b93dde30b682a218e7903c7ac33618389dd33ea4
Monica <monicalagdaan@hotmail.com>
2023-04-04 15:58:26 -0700

    Passing ALL unit tests for wave 1 2 and 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,14 @@
+import uuid
+
+
 class Item:
-    pass
+    def __init__(self, id = None):
+        if id is None:
+            id = uuid.uuid4().int
+        self.id = id
+
+    def get_category(self):
+        return self.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
