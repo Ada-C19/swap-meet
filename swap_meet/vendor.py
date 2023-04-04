188b7ec5b5b0bef269673d46ff4ff7f5774bfec2
Janice <janicenguyenn@gmail.com>
2023-04-04 11:17:48 -0700

    completed vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.item = item
+        self.inventory.append(item)
+
+        return item
+    
+    def remove(self, matching_item):
+        self.item = matching_item
+        
+        if matching_item in self.inventory:
+            self.inventory.remove(matching_item)
+            return matching_item
+        else:
+            return False
