f26aea4b14dc5114aa1092cea414761ba2554911
Kimberly Lantigua <klantigua11@gmail.com>
2023-04-04 16:28:01 -0700

    implemented the Clothing class and passed associated tests in test_wave_05. Interesting glitch with if else in the constructor that I need to bring up with an instructor/ TA

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,21 @@
+from swap_meet.item import Item
+import uuid 
+
 class Clothing:
-    pass
+    
+    # constructor
+    def __init__(self, id = None, fabric = "Unknown"):
+        if not id: 
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+        
+        self.fabric = fabric
+
+    def get_category(self): 
+        return "Clothing"
+    
+    def __str__(self): 
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+    
+    
