7a06e05f3a5534b829ac13321e53d51666f489a6
lulu <lulusun1210@gmail.com>
2023-04-03 22:59:36 -0500

    finish wave 3,4,5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,9 +2,24 @@ import uuid
 
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         random_id = uuid.uuid1().int
         self.id = random_id if id is None else id
+        self.condition = condition
+
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}."
 
     def get_category(self):
-        return "Item"
+        return self.__class__.__name__
+
+    def condition_description(self):
+        condition_dict = {
+            0: "Worn Out",
+            1: "Heavily Used",
+            2: "Normaly Used",
+            3: "Lightly Used",
+            4: "Like New",
+            5: "Brand New"
+        }
+        return condition_dict[self.condition]
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,23 @@ class Vendor:
             if item.id == id:
                 return item
         return None
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.remove(my_item)
+            self.add(their_item)
+            other_vendor.remove(their_item)
+            other_vendor.add(my_item)
+            return True
+        return False
+
+    def swap_first_item(self, other_vendor):
+        if self.inventory and other_vendor.inventory:
+            my_first_item = self.inventory[0]
+            other_first_item = other_vendor.inventory[0]
+            self.remove(my_first_item)
+            self.add(other_first_item)
+            other_vendor.remove(other_first_item)
+            other_vendor.add(my_first_item)
+            return True
+        return False
