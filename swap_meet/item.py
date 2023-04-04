5f893f95480f46dd6666ddf2e2f59f5d1a92e627
Sophia Tran <sophiat8832@gmail.com>
2023-04-04 13:44:35 -0700

    fixed default arg of constructor

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,9 @@
 import uuid
 
 class Item:
-    def __init__(self, id = uuid.uuid4().int):
+    def __init__(self, id = None):
+        if id is None:
+            id = uuid.uuid4().int
         self.id = id
 
     def get_category(self):
