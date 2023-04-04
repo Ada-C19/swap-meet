793baff17dcfd631da42431796a4cbf30b54da11
Ana Silvera <anasilverx@gmail.com>
2023-04-03 19:38:18 -0700

    Override of str method to output a sentence for an Item object

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,10 +2,14 @@ import uuid
 
 class Item:
     def __init__(self, id = None):
-        if id is None:
-            self.id = uuid.uuid4().int
-        else:
+        if id:
             self.id = id
+        else:
+            self.id = uuid.uuid4().int
 
     def get_category(self):
         return self.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+        
