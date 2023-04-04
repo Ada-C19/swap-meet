e19954d03dbebff08e523ae5b9d0e83a8cd3defc
Charlie <charliethemushi@gmail.com>
2023-04-04 12:12:43 -0700

    started wave 1 with partial psuedo and notes

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,11 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        #inventory is list, can pass in
+        self.inventory = inventory or []
+        #add takes in one item and adds it to inventory
+        def add(self, item):
+            self.inventory.append(item)
+        #remove takes in one item and removes the matching item
+        def remove(self, item)
+            if in remove
+            return item
