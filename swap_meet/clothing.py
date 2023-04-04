d7ae1bb88bcfcab705e2c81d3abeabf397fceba1
Sage Choi <coolcuds@gmail.com>
2023-04-04 13:23:27 -0700

    Removed extra blank line.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,11 +1,12 @@
 import uuid
 
 class Clothing:
-    def __init__(self, id=None, fabric="Unknown"):
+    def __init__(self, id=None, fabric="Unknown", condition=0):
         if not id:
             id = uuid.uuid4().int
         self.id = id
         self.fabric = fabric
+        self.condition = condition
 
     def get_category(self):
         return self.__class__.__name__
@@ -13,3 +14,4 @@ class Clothing:
     def __str__(self):
         return (f"An object of type Clothing with id {self.id}. "
                 f"It is made from {self.fabric} fabric.")
+    
