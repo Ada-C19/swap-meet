6962a423aeaca637b43d0da1bb966f50eae59417
Anna Lima <annavanwesep@gmail.com>
2023-04-03 21:37:20 -0700

    first wave passing all tests with assertion added for unit 1 test

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,15 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = inventory or []
+    
+    def add(self, one_item):
+        if not one_item in self.inventory:
+            self.inventory.append(one_item)
+        return one_item
+    
+    def remove(self, one_item):
+        if not one_item in self.inventory:
+            return False
+        elif one_item in self.inventory:
+            self.inventory.remove(one_item)
+            return one_item
