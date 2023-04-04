cc63a9e6374e13b18bedf7a505d92a43542c2607
Madison Jackson <madi.jackson29@gmail.com>
2023-04-03 18:44:28 -0700

    tests 1-4 complete on wave2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,12 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id = None):
+        self.id = uuid.uuid4().int if id is None else id
+        
+    def get_category(self):
+        # return "Item"
+        return (f"{self.__class__.__name__}")
+    
+    def get_by_id(self):
+        
