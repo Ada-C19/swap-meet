85c2746f0275cdf3d77fbd42330f1a8363a30f75
Allie Soliz <solizale@gmail.com>
2023-04-04 16:25:32 -0400

    adds class Item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,11 @@
+import uuid
+
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        self.id = int(uuid.uuid4())
+        print({self.id})
+
+    def get_category(self):
+        class_name = Item()
+        return class_name.__class__.__name__
