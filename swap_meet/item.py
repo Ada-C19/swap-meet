e9f80c2db147104f5b2a3968fcdf91eeb21791df
Sarah Kim <kimsuyon2013@gmail.com>
2023-04-03 17:09:55 -0700

    refactored code to correctly turn uuid into int

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -4,7 +4,7 @@ class Item:
 
     def __init__(self, id= None):
         if not id: 
-            id = int(uuid.uuid4())
+            id = uuid.uuid4().int
         self.id = id 
     
     def get_category(self):
