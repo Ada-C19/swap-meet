9043c61b8cf2e54b564031ced267067ad63b21fe
Sydney Alberson <sydney.alberson@gmail.com>
2023-04-04 14:38:29 -0400

    Finished Wave 04

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -60,13 +60,17 @@ class Vendor:
 #  It returns True
 
 #  If either itself or the friend have an empty inventory, the method returns False
-def swap_first_item(self, other_vendor):
-    my_first_item = self.inventory[0]
-    their_first_item = other_vendor.inventory[0]
-
-    if their_first_item not in self.inventory:
-        self.remove(my_first_item)
-        self.add(their_first_item)
-        other_vendor.remove(their_first_item)
-        other_vendor.add(my_first_item)
-        return True
+    def swap_first_item(self, other_vendor):
+        
+        if self.inventory == [] or other_vendor.inventory == []:
+            return False
+        
+        my_first_item = self.inventory[0]
+        their_first_item = other_vendor.inventory[0]
+        
+        if their_first_item not in self.inventory:
+            self.add(their_first_item)
+            other_vendor.add(my_first_item)
+            other_vendor.remove(their_first_item)
+            self.remove(my_first_item)
+            return True
