4c528757c44150a6aaade8633b8eca249ee8eb7b
Jiaping Chen <jiapingchen17@gmail.com>
2023-04-03 18:06:22 -0700

    added some code in Vendor and Item, still need to import the UUID package

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -15,3 +15,8 @@ class Vendor:
         return False
     
 
+    def get_by_id(self,id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
