52ee4c770e20a46bd5c86f59d833b934f57fcd1c
Agnes Kim <agneskim4@gmail.com>
2023-04-04 16:38:55 -0400

    added electronic class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,11 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, id= None, type = "Unknown"):
+        super().__init__(id)
+        self.type = type
+    
+    # def get_category is already defined in Item
+
+    def __str__(self):
+        return super().__str__() + f" This is a {self.type} device."
