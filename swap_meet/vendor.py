8fd1959bd54fc691d82c81ff486182d24627d01a
Jiaping Chen <jiapingchen17@gmail.com>
2023-04-03 17:36:39 -0700

    added code for wave 1 in Class Vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory =[]):
+        self.inventory = inventory
+
+
+    def add(self, add_item):
+        self.inventory.append(add_item)
+        return add_item
+
+
+    def remove(self, remove_item):
+        if remove_item in self.inventory:
+            self.inventory.remove(remove_item)
+            return remove_item
+        return False
+    
+
