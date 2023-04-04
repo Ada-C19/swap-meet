b249766f92d9da9fb6ef2616e898080636364ddf
Lily <aulily09@gmail.com>
2023-04-04 11:21:18 -0700

    Completed condition_description()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,8 +5,8 @@ class Item:
         self.id = id
         if id is None:
             self.id = int(uuid.uuid4())
-
-        self.condition = condition 
+    
+        self.condition = condition
 
     def get_category(self):
         return str(self.__class__.__name__)
@@ -14,3 +14,16 @@ class Item:
     def __str__(self):
         return f"An object of type Item with id {self.id}."
     
+    def condition_description(self):
+        if self.condition == 0:
+            return "Unusuable"
+        elif self.condition == 1:
+            return "Heavily used, almost unusable"
+        elif self.condition == 2:
+            return "Terrible"
+        elif self.condition == 3:
+            return "Meh"
+        elif self.condition == 4:
+            return "Decent"
+        else:
+            return "Excellent condition"
