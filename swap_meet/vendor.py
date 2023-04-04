996321a7c84b0f3e6812bfd82e4ac4a8fde0e054
Monica <monicalagdaan@hotmail.com>
2023-04-03 19:20:21 -0700

    now passes 4/5 wave 1 unit tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,7 @@
 class Vendor:
 
-    def __init__(self):
-        self.inventory = []
+    def __init__(self, inventory = []):
+        self.inventory = inventory
 
     def add(self, item):
         self.inventory.append(item)
