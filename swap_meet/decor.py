7aa509e03f24e503e0adf622a0d72efce133912c
jasmine <jtmsoto@gmail.com>
2023-04-04 15:13:34 -0700

    Add condition attribute to item, decor, electronics, clothing classes and get_condition function

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -3,8 +3,8 @@ from swap_meet.item import Item
 
 
 class Decor(Item):
-    def __init__(self, id=None, width=0, length=0):
-        super().__init__(id)
+    def __init__(self, id=None, width=0, length=0, condition=0):
+        super().__init__(id, condition)
         self.width = width
         self.length = length
 
@@ -12,5 +12,5 @@ class Decor(Item):
         return "Decor"
     
     def __str__(self):
-        return (f"{super().__str__()} "
-                f"It takes up a {self.width} by {self.length} sized space.")
+        return (f"{super().__str__()} It takes up a "
+                f"{self.width} by {self.length} sized space.")
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,16 +1,33 @@
 from uuid import uuid4
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         if id is not None and isinstance(id, int):
             self.id = id
         else:
             id = uuid4()
             id = id.int
             self.id = id
+        self.condition = condition
 
     def get_category(self):
         return "Item"
     
     def __str__(self):
-        return f"An object of type Item with id {self.id}."
+        return f"An object of type {self.get_category()} with id {self.id}."
+    
+    def condition_description(self):
+        condition = round(self.condition)
+        
+        if condition == 5:
+            return "Brand New"
+        if condition == 4:
+            return "Like New"
+        if condition == 3:
+            return "Very Good"
+        if condition == 2:
+            return "Good"
+        if condition == 1:
+            return "Acceptable"
+        else:
+            return "As Is"
