9c02a6074f260f7cb769d1ecad3866f3b6fa1ee3
nadia <reach.nadia.saria@gmail.com>
2023-04-03 18:13:09 -0700

    completes wave 03

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -4,5 +4,9 @@ class Item:
     def __init__(self, id=None):
         self.id = uuid4().int if id is None else id
 
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+
     def get_category(self):
         return "Item"
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,21 @@ class Vendor:
             if item.id == object_id:
                 return item
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or\
+        their_item not in other_vendor.inventory:
+            return False
+        
+        # remove my_item from this Vendor
+        self.remove(my_item)
+        # add my item to friend's inventory
+        other_vendor.add(my_item)
+        # remove their item from that Vendor
+        other_vendor.remove(their_item)
+        # add their item to my inventory
+        self.add(their_item)    
+        return True
+            
+        # if either item not in inventory
+        # return False 
