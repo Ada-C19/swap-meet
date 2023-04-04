e4e2d2a083bdbc2184a0eee2f028b76342deb2e9
Stacey <stacey.l.joseph@gmail.com>
2023-04-04 13:01:51 -0400

    Wave 5, clothing module complete

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,27 @@
+import uuid
+
 class Clothing:
-    pass
+    # Has an attribute id that is by default a unique integer
+        # will unique integer by separate from uuid from integer?
+    
+    # Has an attribute fabric that is by default the string "Unknown"
+    
+    def __init__(self, id=None, fabric = "Unknown"):
+        
+        # using UUID4 since UUID1 creates a UUID with the computer's network address 
+        # UUID.int- returns the UUID as a 128-bit integer.
+        # self.id = uuid.uuid4().int
+        self.id =  id if id is not None else uuid.uuid4().int
+        
+        # following a similar syntax for fabric as id
+            #  eventually fabric values may be :
+            # new_clothing = Clothing(1234, "Striped") ..."Cotton", or "Floral"
+        self.fabric = fabric if fabric is not "Unknown" else fabric
+        
+    # Has a function get_category that returns "Clothing
+    def get_category(self):
+        return self.__class__.__name__
+    
+    # Has a stringify method that returns 
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
