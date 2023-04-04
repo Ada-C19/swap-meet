829d0214cab78ff82f95c46c87c213f9dcce343e
Sage Choi <coolcuds@gmail.com>
2023-04-04 13:24:18 -0700

    Added constructor and instance methods to Electronics class.

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,17 @@
+import uuid
+
 class Electronics:
-    pass
+    def __init__(self, id=None, type="Unknown", condition=0):
+        if not id:
+            id = uuid.uuid4().int
+        self.id = id
+        self.type = type
+        self.condition = condition
+
+    def get_category(self):
+        return self.__class__.__name__
+    
+    def __str__(self):
+        return (f"An object of type Electronics with id {self.id}. "
+                f"This is a {self.type} device.")
+    
