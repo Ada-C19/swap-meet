afaf4f0e0b3e4f823c3c3f3ac37d7c5989431a6c
Abby <argoodm@gmail.com>
2023-04-04 15:09:44 -0400

    electronics final

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,16 @@
+import uuid
+    
 class Electronics:
-    pass
+
+    def __init__(self, id=None, type="Unknown"):
+        self.id = id if id is not None else int(uuid.uuid4())
+        self.type = type
+    
+
+
+
+    def get_category(self):
+        return "Electronics"
+
+    def __str__(self):
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
