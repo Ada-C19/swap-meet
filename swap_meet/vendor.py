f7606fde0407c4a736130b675f42c2869cd86bc2
jasmine <jtmsoto@gmail.com>
2023-04-04 14:23:34 -0700

    Add swap_first_item method to Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -24,7 +24,7 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         # Checks corresponding items are in inventories
         if (my_item not in self.inventory or 
-            their_item not in other_vendor.inventory):
+                their_item not in other_vendor.inventory):
             return False
         
         # Gives vendor's item to other vendor
@@ -34,5 +34,14 @@ class Vendor:
         # Receives other vendor's item
         self.inventory.append(their_item)
         other_vendor.inventory.remove(their_item)
+
+        return True
+    
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        
+        self.inventory.append(other_vendor.inventory.pop(0))
+        other_vendor.inventory.append(self.inventory.pop(0))
 
         return True
