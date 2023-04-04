7100bd082fc094af2cad945a201db5d0a92dd7a6
Julie Loh <jlohusky@gmail.com>
2023-04-04 11:30:10 -0700

    class Clothing child class of class Item

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,19 @@
-class Clothing:
-    pass
+import uuid
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, id=None, fabric=None):
+        super().__init__(id)
+
+        if not fabric:
+                fabric = "Unknown"
+        self.fabric = fabric
+    
+    def __str__(self):
+        return (super().__str__() + ' It is made from ' + 
+                str(self.fabric) + ' fabric.')
+    
+# clothing = Clothing(id=12345)
+# print(clothing.id)
+# str_clothing_id = str(clothing.id)
+# print(len(str_clothing_id))
