0dd091b3ae28e090cf0188c390e96baa05ce8137
Lyuda Kim <lyudarkim@gmail.com>
2023-04-03 20:34:09 -0700

    Wrote Decor class, passing 3/4 tests.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,14 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+
+class Decor(Item):
+        
+    def __init__(self, id, width=0, length=0):
+        super().__init__(id)
+        self.id = id
+        self.width = width
+        self.length = length
+
+    def __str__(self):
+        category_id_description = super().__str__()
+        return f"{category_id_description} It takes up a {self.width} by {self.length} sized space."
