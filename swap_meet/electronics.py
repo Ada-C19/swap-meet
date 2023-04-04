a0d460657436cd127af8c813cf4ce60b352ce263
Susi Franco <susi.franco32@gmail.com>
2023-04-04 08:28:43 -0700

    added electronics class that inherits from Item class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,19 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+class Electronics(Item):
+    def __init__(self, id=None, type=None):
+        #if id is none, check at parent class and create id.
+        if id is None:
+            super().__init__()
+        #otherwise, use given id as id.
+        else:
+            super().__init__(id=id)
+
+        if type is None:
+            self.type = "Unknown"
+        else:
+            self.type = type
+    #override parent stringify to add the second portion of string."  
+    def __str__(self):
+        return f"An object of type {self.__class__.__name__} with id {self.id}. This is a {self.type} device."
+    
+    
