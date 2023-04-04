8bbe183caab258ce20af0f5353106dce98fcffbf
Jiaping Chen <jiapingchen17@gmail.com>
2023-04-03 18:38:23 -0700

    added more code in vendor and item, corrected test case in wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -6,5 +6,11 @@ class Item:
         else:
             self.id = id
     
+
     def get_category(self):
         return "Item"
+    
+
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -20,3 +20,13 @@ class Vendor:
             if item.id == id:
                 return item
         return None
+
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.remove(my_item)
+            self.add(their_item)
+            other_vendor.remove(their_item)
+            other_vendor.add(my_item)
+            return True
+        return False
