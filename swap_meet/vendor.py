c44123ce6636d182b37d4c7d8381cfd7e333d9a1
Kimberly Lantigua <klantigua11@gmail.com>
2023-04-04 14:44:53 -0700

    Defined swap_first_item method in Vendor Class and passed all tests in test_wave_04.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -39,3 +39,20 @@ class Vendor:
             return True
         
         return False
+    
+    # method to swap first items between two vendors
+    def swap_first_item(self, other_vendor): 
+        if not self.inventory or not other_vendor.inventory: 
+            return False
+        
+        
+        my_first_item = self.inventory[0]
+        their_first_item = other_vendor.inventory[0]
+        
+        self.remove(my_first_item)
+        self.add(their_first_item)
+
+        other_vendor.remove(their_first_item)
+        other_vendor.add(my_first_item)
+
+        return True
