7ea349b54a446b34c87710bc5ac668674df87613
Airis <airis.wang2014@fgvmail.br>
2023-04-04 15:54:42 -0400

    defined get_by_category(), get_best_by_category(), swap_best_by_category() and passed wave 06

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -41,3 +41,31 @@ class Vendor:
             other_vendor.remove(other_vendor.inventory[0])
 
             return True
+        
+    def get_by_category(self, category):
+        result = []
+        for item in self.inventory:
+            if item.get_category() == category:
+                result.append(item)
+        return result
+    
+    def get_best_by_category(self, category):
+        filtered_items = {}
+
+        for item in self.inventory:
+            if item.get_category() == category:
+                filtered_items[item.condition] = item
+        
+        for item in filtered_items:
+            return filtered_items[max(filtered_items)]
+        
+
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other_vendor.get_best_by_category(my_priority)
+
+        if not my_best_item or not their_best_item:
+            return False
+        else:
+            self.swap_items(other_vendor, my_best_item, their_best_item)
+            return True
