af5b00c768f02ece3be3792880ca48f9619485de
April Zhang <aprilzh23@gmail.com>
2023-04-04 14:38:05 -0700

    create swap_items function of Vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,6 @@
 from swap_meet.item import Item
 class Vendor:
     def __init__(self, inventory=None): 
-        #Why cannot pass test without inventory?
         if inventory is None:
             inventory = []
         self.inventory = inventory
@@ -20,6 +19,15 @@ class Vendor:
     def get_by_id(self, id):
         for item in self.inventory:
             if id == item.id:           
-            # item.id Is this`item` refers to the string in the inventory?
                 return item
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        else:
+            self.remove(my_item)
+            other_vendor.add(my_item)
+            other_vendor.remove(their_item)
+            self.add(their_item)
+            return True
