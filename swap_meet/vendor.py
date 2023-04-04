396084d3a7098995183e07f4d42e2842b8da6787
Jackie <jackiemay08@gmail.com>
2023-04-04 16:12:46 -0400

    second wave commit

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,30 @@
+from swap_meet.item import Item
+
 class Vendor:
-    # attribute -> inventory == []
+
     def __init__(self, inventory = None):
         self.inventory = [] if inventory is None else inventory
-    # method -> takes in item, adds item to inventoryattribute, returns item 
+
     def add(self, item): 
         self.inventory.append(item)
         return item
-    # method -> takes in item, removes the matching item, returns item
-    #        -> if no match, return False
+
     def remove(self, item):
-        if item in self.inventory:
-            self.inventory.remove(item)
-            return item
-        return False
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+
+# - Instances of `Vendor` have an instance method named `get_by_id`
+#   - This method takes one argument: an integer, representing an `Item`'s `id`
+#   - This method returns the item with a matching `id` from the inventory
+#   - If there is no matching item in the `inventory`, the method should explicitly return `None`
+    def get_by_id(self, id_num):
+
+        for item in self.inventory:
+            if item.id and item.id == id_num:
+                #return the value stored in the variable "item" 
+                return item
+        else:
+            return None
+
