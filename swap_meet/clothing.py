eed351b65f6d9c896e4552ca62c96411e7256435
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-04-04 05:15:47 -0600

    Added electronics. Research category in items

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,8 +1,10 @@
 from swap_meet.item import Item
-class Clothing:
-    def __init__(self, category="Clothing", condition=0, fabric="Unknown"):
-        super().__init__(category, condition) #check viability of super 
+class Clothing(Item):
+    def __init__(self, category="Clothing", fabric="Unknown"):
+        super().__init__(category) #check viability of super 
     def __str__(self):
         return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
 
+    def get_category(self):
+        return f"{self.category}"
     
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,11 @@
+from swap_meet.item import Item
 class Electronics:
-    pass
+    def __init__(self, category="Electronics", type="Unknown"):
+        super().__init__(category)
+        self.type = type
+        
+    def __str__(self):
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
+    
+    def get_category(self):
+        return f"{self.category}"
