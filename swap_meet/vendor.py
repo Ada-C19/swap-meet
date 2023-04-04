ba8133d207f8606090f1daae549664ff533ba865
Raissa Barbosa <raissaglaucie@icloud.com>
2023-04-04 09:09:27 -0400

    Wave 03 and 04 is done

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,26 @@ class Vendor:
             if item.id == id:
                 return item
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.remove(my_item)
+            self.add(their_item)
+            other_vendor.remove(their_item)
+            other_vendor.add(my_item)
+            return True
+        return False
+    
+    def swap_first_item(self, other_vendor):
+        if self.inventory and other_vendor.inventory:
+            my_item = self.remove(self.inventory[0])
+            their_item = other_vendor.remove(other_vendor.inventory[0])
+            self.add(their_item)
+            other_vendor.add(my_item)
+            return True
+        return False
+
+
+
+
+
