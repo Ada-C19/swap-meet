42f252047eea652ebabfb7479827ce1174102d59
Sophia Tran <sophiat8832@gmail.com>
2023-04-04 15:30:31 -0700

    implemented swap_first_item

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -33,3 +33,10 @@ class Vendor:
         self.add(their_item)
 
         return True
+    
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        
+        self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
+        return True
