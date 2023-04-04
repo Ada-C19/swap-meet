e9b94715a3bdfe06541d013045bde980cd65dbc8
Ariel Williams <arielwilliams121013@gmail.com>
2023-04-04 15:37:16 -0700

    Wrote logic for wave 1. Passing 4/5 tests for wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,15 @@
 # Defining class Vendor
 class Vendor:
-    pass
+    # inventory is an empty list
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        if item:
+            self.inventory.append(item)
+            return item
+        
+    def remove(self, item):
+        if item == item:
+            self.inventory.remove(item)
+            return item
