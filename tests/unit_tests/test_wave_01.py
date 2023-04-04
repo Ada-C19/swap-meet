a08f7b49ed3734fb2ce78dc79af3293a63390850
Charlie <charliethemushi@gmail.com>
2023-04-04 13:17:31 -0700

    finished assert in test wave1

--- tests/unit_tests/test_wave_01.py
+++ tests/unit_tests/test_wave_01.py
@@ -50,6 +50,7 @@ def test_removing_not_found_is_false():
 
     result = vendor.remove(item)
 
+    assert result == False 
     #raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
