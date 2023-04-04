4dbcb91d6312f8245074175abbc1f8fdce410998
Selene Castillo <secast@live.com>
2023-04-04 14:24:05 -0700

    ropita in wave 5 complete

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,14 @@
+import uuid
+
 class Clothing:
-    pass
+    def __init__(self, id = None, fabric = "Unknown"):
+        if not id:
+            id = uuid.uuid1().int
+        self.id = id
+        self.fabric = fabric
+
+    def get_category(self):
+        return "Clothing"
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
