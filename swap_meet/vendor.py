965567e5f649734bc6b0acb54c73e8b9de61e324
Divya <divya.naidoo5@gmail.com>
2023-04-04 09:43:28 -0700

    completed wave 1 and got tests to pass

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if not item in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item
