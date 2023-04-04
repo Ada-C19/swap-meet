419f7cd95a3a2372b3122995a9a2fc53d30cfec2
Sydney Alberson <sydney.alberson@gmail.com>
2023-04-04 15:00:13 -0400

    Created Clothing Class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,25 @@
-class Clothing:
-    pass
+import uuid
+from swap_meet.item import Item
+
+"""
+    Has an attribute id that is by default a unique integer
+    Has an attribute fabric that is by default the string "Unknown"
+        This attribute describes what fabric the clothing is made from; some example values might be "Striped", "Cotton", or "Floral"
+        When we instantiate an instance of Clothing, we can optionally pass in a string with the keyword argument fabric
+    Has a function get_category that returns "Clothing"
+    Has a stringify method that returns "An object of type Clothing with id <id value>. It is made from <fabric value> fabric."
+        For example, if we had a Clothing instance with an id of 123435 and a fabric attribute that holds "Wool", its stringify method should return "An object of type Clothing with id 12345. It is made from Wool fabric."
+
+"""
+
+class Clothing(Item):
+    
+    def __init__(self, id= None, fabric= "Unknown", condition= 0):
+        self.id = id if id is not None else int(uuid.uuid4())
+        self.fabric = fabric
+
+    def get_category(self):
+        return "Clothing"
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,27 +1,29 @@
 import uuid
 
+"""
+    In Wave 2 we will create the Item class and the Vendor class' get_by_id method.
+    There is a module (file) named item.py inside of the swap_meet package (folder)
+    Inside this module, there is a class named Item
+    Each Item will have an attribute named id, which is a unique integer by default
+    There are many ways to generate numbers, but generating numbers without duplicates takes some care. 
+    Happily, Python has a package called uuid that can help!
+    With a little research we can use one of the functions uuid provides to create large unique numbers meant to be used as identifiers
+    Specifically, you'll need to choose which of the uuid package's functions to use, so be sure to consider which function will work best for creating a unique integer
+    Note that this package's functions return UUID objects, not integers as such, but UUID objects have an attribute int which allow us to access their value as an integer
+    When we initialize an instance of Item, we can optionally pass in an integer with the keyword argument id to manually set the Item's id
+    Each Item will have a function named get_category, which will return a string holding the name of the class
+    Instances of Vendor have an instance method named get_by_id
+    This method takes one argument: an integer, representing an Item's id
+    This method returns the item with a matching id from the inventory
+    If there is no matching item in the inventory, the method should explicitly return None
+
+"""
+
 class Item:
-    # In Wave 2 we will create the Item class and the Vendor class' get_by_id method.
-    # There is a module (file) named item.py inside of the swap_meet package (folder)
-     # Inside this module, there is a class named Item
-    # Each Item will have an attribute named id, which is a unique integer by default
-    # There are many ways to generate numbers, but generating numbers without duplicates takes some care. 
-    # Happily, Python has a package called uuid that can help!
-   # With a little research we can use one of the functions uuid provides to create large unique numbers meant to be used as identifiers
-    # Specifically, you'll need to choose which of the uuid package's functions to use, so be sure to consider which function will work best for creating a unique integer
-    # Note that this package's functions return UUID objects, not integers as such, but UUID objects have an attribute int which allow us to access their value as an integer
-     # When we initialize an instance of Item, we can optionally pass in an integer with the keyword argument id to manually set the Item's id
-    # Each Item will have a function named get_category, which will return a string holding the name of the class
-     # Instances of Vendor have an instance method named get_by_id
-    # This method takes one argument: an integer, representing an Item's id
-    # This method returns the item with a matching id from the inventory
-    # If there is no matching item in the inventory, the method should explicitly return None
-
-
 
     # def add_to_list_ok(word, word_list=None):
     # word_list = word_list if word_list is not None else []
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition= 0.0):
         # self.item = item
         self.id = id if id is not None else int(uuid.uuid4())
 
