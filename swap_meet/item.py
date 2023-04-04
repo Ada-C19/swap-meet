618c8d04f9a5c04cad96dbe838586ea66a21f1b4
Edith <mxedithbaltazar@gmail.com>
2023-04-03 22:22:34 -0700

    wave 02 import uuid

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,26 +1,16 @@
 # There is a module (file) named item.py inside of the swap_meet package 
 # (folder)
-
+import uuid
 # Inside this module, there is a class named Item
 class Item:
-    pass
-  
+    
 
-# Each Item will have an attribute named id, which is a unique integer 
-# by default
+    def __init__(self, id=0):
+        if id is 0:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
 
-# There are many ways to generate numbers, but generating numbers 
-# without duplicates takes some care. Happily, Python has a package 
-# called uuid that can help!
-# If we import the uuid package in item.py, with a little research 
-# we can use one of the functions uuid provides to create large 
-# unique numbers meant to be used as identifiers
-# Specifically, you'll need to choose which of the uuid package's 
-# functions to use, so be sure to consider which function will work best 
-# for creating a unique integer
-# Note that this package's functions return UUID objects, not integers 
-# as such, but UUID objects have an attribute int which allow us to 
-# access their value as an integer
-# When we initialize an instance of Item, we can optionally pass in an 
-# integer with the keyword argument id to manually set the Item's 
-# id
+    def get_category(self):
+        return self.__class__.__name__
+
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,50 +1,34 @@
 #   In Wave 1 we will create the Vendor class.
 class Vendor:
     
-        # Each Vendor will have an attribute named inventory, which is an empty list by default
-        # When we instantiate an instance of Vendor, we can optionally pass in a list with the 
-                    # keyword argument inventory
+
     def __init__(self, inventory = None):
         if inventory is None:
             inventory = []
         self.inventory = inventory
     
-    # Every instance of Vendor has an instance method named add, which takes in one item
-    # This method adds the item to the inventory
     def add(self, item):
         self.inventory.append(item)
         # This method returns the item that was added
         return item
 
-    # Similarly, every instance of Vendor has an instance method named remove, 
-    # which takes in one item
-    # This method removes the matching item from the inventory
     def remove(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
             # This method returns the item that was removed
             return item
         else:
-        # If there is no matching item in the inventory, 
-        # the method should return False
+
             return False
-        
-# In Wave 2 we will create the Item class and the Vendor class'
-#  get_by_id method.
-   #def get_by_id():
 
-
-
-
-#Each Item will have a function named get_category, which will return
-#  a string holding the name of the class
 
 #Instances of Vendor have an instance method named get_by_id
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
 
-# This method takes one argument: an integer, representing an Item's id
-# This method returns the item with a matching id from the inventory
-# If there is no matching item in the inventory, the method should 
-# explicitly return None
 
         
 
