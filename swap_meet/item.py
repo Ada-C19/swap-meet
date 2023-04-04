851341a0205202ff1f5132c6aa7986b89863760e
Lily <aulily09@gmail.com>
2023-04-03 17:06:26 -0700

    Functional __init__() and get_category()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,10 @@
+import uuid
 class Item:
-    pass
+    
+    def __init__(self, id=0):
+        self.id = id
+        if self.id == 0:
+            self.id = int(uuid.uuid4())
+
+    def get_category(self):
+        return str(self.__class__.__name__)
