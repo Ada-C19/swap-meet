47e49183e1bfa8b85b6a00acccc6cfdcf1d3fcdf
louloera <louloera@gmail.com>
2023-04-03 21:56:13 -0700

    Added new attribute condition to Item and updated child classes

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,8 +2,8 @@ from swap_meet.item import Item
 
 
 class Clothing(Item):
-    def __init__(self, id=None,fabric='Unknown'):
-        super().__init__(id)
+    def __init__(self, id=None,condition=0,fabric='Unknown'):
+        super().__init__(id,condition)
         self.fabric = fabric
     def __str__(self):
         return super().__str__() + f" It is made from {self.fabric} fabric."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,3 +1,8 @@
 from swap_meet.item import Item
 class Electronics(Item):
-    pass
+    def __init__(self, id=None,condition=0,type="Unknown"):
+        super().__init__(id,condition)
+        self.type=type
+    
+    def __str__(self):
+        return super().__str__() + f" This is a {self.type} device."
