9a3d780f12e8bb8d819531daf23104b955ade687
Carolyn Qi <qi.carolyn@gmail.com>
2023-04-04 13:39:39 -0700

    Deleted test pytest for test 1, 2, and 3, and added wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,13 @@
+import uuid
 class Item:
-    pass
+    def __init__(self, id = None):
+        if not id:
+            id = uuid.uuid4().int
+        self.id = id
+        print(self.id)
+
+    def __str__(self):
+        return f"An object of type {self.get_category()} with id {self.id}."
+
+    def get_category(self):
+        return "Item"    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,4 @@
+from swap_meet.item import Item
 class Vendor:
     def __init__(self, inventory=None):
         if not inventory:
@@ -14,3 +15,24 @@ class Vendor:
             return item
         except ValueError:
             return False
+        
+    def get_by_id(self,id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        try:
+            self.inventory.remove(my_item)
+        except ValueError:
+            return False
+        try:
+            other_vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            other_vendor.inventory.append(my_item)
+        except ValueError:
+            self.inventory.append(my_item)
+            return False
+        return True
+
