a2f1e66c6899098371861fc6f0b7ddbe4694a4b4
Pha'lesa Patton <ppatton86@gmail.com>
2023-04-04 14:34:34 -0400

    disregard

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,13 @@
+from swap_meet.item import Item
+
 class Clothing:
-    pass
+    def __init__(self,fabric = "Uknown"):
+        super().__init__(id)
+        self.fabric = fabric
+
+    def get_category(self):
+        return 
+
+    
+
+    
