aee16883e28331d79daeea58f9708fe2ea221226
Carline <carlinesenat@gmail.com>
2023-04-04 15:33:01 -0700

    Working on Wave 2!

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
+import uuid
+# from swap_meet.vendor import Vendor
+
 class Item:
-    pass
+    def __init__(self,id=0): 
+    #    self.id = int(uuid.uuid4()) if id == 0 else self.id = id         #if id > 0:
+        if id == 0:
+            self.id = int(uuid.uuid4())
+        else:
+            self.id = id
+    
+    def get_category(self):
+        item = Item()
+        return item.__class__.__name__
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from swap_meet.item import Item
+
 class Vendor:
     def __init__(self, inventory = None):
         self.inventory = [] if inventory is None else inventory 
@@ -6,14 +8,17 @@ class Vendor:
         self.inventory.append(item)
         return item
     
-        
-        
-    
     def remove(self,item):
         if item not in self.inventory:
             return False
         else:
             self.inventory.remove(item)
             return item 
-            
+    
+    def get_by_id(self,id):
+        id = Item(id) 
+        if id in self.inventory:
+            return self.id
+        else: 
+            return self.id is None 
             
