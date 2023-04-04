0372a28fbabe5b60110bc9d9f50c799dc4e0a00c
Tigist Woldearegai <tigistlina@gmail.com>
2023-04-03 22:05:06 -0500

    defines Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,12 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if inventory is None:
+            self.inventory  =  []
+        else:
+            self.inventory  = inventory
+            
+    def add(self, item):
+        self.inventory.append(item)
+        
+    def remove(self, item):
+        self.inventory.index(item).remove(item)
