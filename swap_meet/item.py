0fda4eeafc0d1452994909fa0895a534732f3c16
Abby Castillo <abby.machines@gmail.com>
2023-04-05 14:49:01 -0700

    successfully implemented condition_description() method in item.py, passed all wave 5 tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+import math
 
 class Item:
     def __init__(self, id=None, condition=0):
@@ -12,3 +13,13 @@ class Item:
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}."
     
+    def condition_description(self):
+        CONDITION_DESCRIPTIONS = { 0: "awful. the worst item you can possibly imagine",
+                                  1: "could be worse",
+                                  2: "could be better",
+                                  3: "fine. do u really want this tho",
+                                  4: "pretty good tbqh",
+                                  5: "get it before someone else does!!!!!!!!"}
+        formatted_condition_value = math.floor(self.condition)
+        
+        return CONDITION_DESCRIPTIONS[formatted_condition_value]
