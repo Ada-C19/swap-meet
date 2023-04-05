74bf6f375f96d7eb631c688bfb11757ee2aac1ec
chickenandegg <tei.villanueva@gmail.com>
2023-04-04 22:37:44 -0700

    import uuid to generate random unique numbers

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,12 @@
+import uuid
+
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            self.id = str(uuid.uuid4())
+
+    def get_category(self, item):
+        return item
