6628be4afa263389a25cec2751971b2580dd02ee
Jackie <jackiemay08@gmail.com>
2023-04-05 17:23:54 -0400

    WIP wave 5

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,7 +1,6 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-<<<<<<< HEAD
     pass
     def __init__(self, id=None, width=0, length=0):
         super().__init__(id)
@@ -13,10 +12,7 @@ def get_category(self):
 
 
 def __str__(self):
-    type_and_id_msg = super().__str__()
+    # type_and_id_msg = super().__str__()
     width_and_length_msg = f"It takes up a {self.width} by {self.length} sized space."
     return f"{type_and_id_msg}{width_and_length_msg}"
     
-=======
-    pass
->>>>>>> 1b69ed7e0962ae8895d004327e672a5b88cd82f3
