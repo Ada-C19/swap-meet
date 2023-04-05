7e29b7eaacb3c699bf5cd63159dc4d6a509db126
Sarah Kim <kimsuyon2013@gmail.com>
2023-04-04 20:49:52 -0700

    added condition attribute and condition description method

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,10 +1,10 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self, width=0, length=0, id=None):
+    def __init__(self, width=0, length=0, id=None, condition=0):
         self.width = width 
         self.length = length 
-        super().__init__(id)
+        super().__init__(id, condition)
 
     def __str__(self): 
         return f"{super().__str__()} It takes up a {self.width} by {self.length} sized space."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,15 +2,25 @@ import uuid
 
 class Item:
 
-    def __init__(self, id= None):
+    def __init__(self, id= None, condition=0):
         if not id: 
             id = uuid.uuid4().int
         self.id = id 
+        self.condition = condition
     
     def __str__(self): 
-        return "An object of type Item with id "+(str(self.id))+"."
+        return f"An object of type {self.get_category()} with id {self.id}."
     
     def get_category(self):
         return self.__class__.__name__
     
-    
+    def condition_description(self):
+        self.item_condition = {5: "Brand new with tag/box/case",
+                               4: "Like new but not quite",
+                               3: "Gently used condition", 
+                               2: "May come with flaws", 
+                               1: "Comes with many flaws",
+                               0: "You sure you want to buy this?"}
+        
+        return self.item_condition[self.condition]
+        
