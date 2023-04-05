eee6a6de5f2a77da352f55530ed0290edc1dcd6a
Sophia Tran <sophiat8832@gmail.com>
2023-04-04 22:01:07 -0700

    imported Item class and created constructor for Decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,7 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+class Decor(Item):
+    def __init__(self, id=None, width=0, length=0):
+        super().__init__(id)
+        self.width = width
+        self.length = length
