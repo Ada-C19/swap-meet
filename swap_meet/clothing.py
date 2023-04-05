3e9a9812eb1101f2482eeab1d4596a02519865b5
Virginia Ramos <virginia.rare@gmail.com>
2023-04-04 20:36:28 -0500

    Start of Wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,10 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+class Clothing(Item):
+    def _init_(self, fabric="Unknown"):
+        self.fabric = fabric 
+    def __str__(self): 
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric." 
+                
+
+
+    
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,15 +2,33 @@ import uuid
 
 class Item:
 
-    def __init__(self, id = None):
+    def __init__(self, id = None, condition=0):
         if id is not None:
             self.id = id
         else:
             self.id = uuid.uuid4().int
+        self.condition = condition
     
     def get_category(self):
         return self.__class__.__name__
     
     def __str__(self):
         return f"An object of type Item with id {self.id}."
+    
+    def condition_description(self):
+        if self.condition == 0: 
+            return "Scrap"
+        if self.condition == 1: 
+            return "Poor" 
+        if self.condition == 2:
+            return "Well loved!"
+        if self.condition == 3: 
+            return "Gently used"
+        if self.condition == 4: 
+            return "Like new"
+        if self.condition == 5: 
+            return "Mint condition"
+
+
         
+    
