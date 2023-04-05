9b75e9342920702a9cd868798c496d030887af5d
Sophia Tran <sophiat8832@gmail.com>
2023-04-04 22:18:42 -0700

    added condition as an optional parameter for all classes

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,11 @@
 import uuid
 
 class Item:
-    def __init__(self, id = None):
+    def __init__(self, id=None, condition=0):
         if id is None:
             id = uuid.uuid4().int
         self.id = id
+        self.condition = condition
 
     def get_category(self):
         return "Item"
