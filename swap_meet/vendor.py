2aae81258c925153079a7b8e26028d1cbaf6141a
celina-barron <celinahas@gmail.com>
2023-04-04 17:20:08 -0700

    all but 10, 13 & 14 tests passing in wave 5

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -25,12 +25,12 @@ class Vendor:
     def get_by_category(self, category):
         matching_items = []
     
-    if category in self.categories:
-        for item in self.inventory:
-            if item.category == category:
-                matching_items.append(item)
-    
-    return matching_items
+        if category in self.categories:
+            for item in self.inventory:
+                if item.category == category:
+                    matching_items.append(item)
+        
+        return matching_items
 
         
     def get_best_by_category(self, category):
@@ -39,7 +39,7 @@ class Vendor:
         for item in self.inventory:
             if item.category() == category and item.condition>highest_condition:
                 best_item = item
-        return best_item
+            return best_item
             
         if items.condition > highest_condition:
                 highest_condition = items.condition
