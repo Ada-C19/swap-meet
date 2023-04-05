e6a9e8e04df2920e930ba7a830ec76cdc35dec26
Jiaping Chen <jiapingchen17@gmail.com>
2023-04-05 14:43:30 -0700

    refactor vendor.py for the last function

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -67,7 +67,7 @@ class Vendor:
     def swap_best_by_category(self, other_vendor, my_priority, their_priority):
         my_best_item = self.get_best_by_category(their_priority)
         their_best_item = other_vendor.get_best_by_category(my_priority)
-        if my_best_item is None or their_best_item is None:
-            return False
+        # if my_best_item is None or their_best_item is None:
+        #     return False
         return self.swap_items(other_vendor, my_best_item, their_best_item)
 
