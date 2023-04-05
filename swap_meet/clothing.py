2fc11fe8eba0edfaba71e6a619881be7dd50197b
Carline <carlinesenat@gmail.com>
2023-04-05 15:19:55 -0700

    Wave 4 and tests complete!

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,3 +1,16 @@
-class Clothing:
-    pass
-#Clothing test
+from .item import Item
+
+
+class Clothing(Item):
+    def __init__(self, fabric):
+        super().__init__(self,id=0)
+
+        self.fabric = "Unknown" if fabric is None else fabric 
+        
+        
+    def get_category(self):
+        item = Item()
+        return item.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
