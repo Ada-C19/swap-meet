3f0f5d873f757c6781d6746630e43f1c03e0f5f9
Yulia S <startyupp@gmail.com>
2023-04-04 22:29:56 -0700

    Created the state and bahaviour of vendor class.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,8 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = [] if inventory is None else inventory
+        
+    def remove(self, item):
+        if not self.inventory or item not in self.inventory:
+            return False
+        return item
