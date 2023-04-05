7976690a1cfbf0eb3010a8338990abbbd8e981ed
Jackie <jackiemay08@gmail.com>
2023-04-05 18:14:16 -0400

    Wave 5 commit

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,4 +1,11 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    pass
+    def __init__(self, id=None, condition=0, type="Unknown"):
+        super().__init__(id, condition)
+        self.type = type
+
+    def __str__(self):
+        original_str = super().__str__()  
+        return f"{original_str} This is a {self.type} device."
+   
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,9 @@
 from uuid import uuid4
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         self.id = uuid4().int if id is None else id
+        self.condition = condition
 
     def get_category(self):
         return self.__class__.__name__
@@ -10,4 +11,19 @@ class Item:
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}."
     
+    def condition_description(self):
+        if self.condition > 4:
+            condition_str = "Near mint. That's a good pick!"
+        elif self.condition > 3:
+            condition_str = "Decent pick. Almost near mint."
+        elif self.condition > 2:
+            condition_str = "Moderately used. It's aight."
+        elif self.condition > 1:
+             condition_str = "You might want to reconsider."           
+        else:
+            condition_str = "This is TRAASHHHHH!!"
+        
+        return condition_str
+
+
 
