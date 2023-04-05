4b9bf47d81774ffd01381e077f28261647da0b94
Jackie <jackiemay08@gmail.com>
2023-04-05 17:18:18 -0400

    [WIP] wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,12 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+
+    def __init__(self, id=None, fabric = "Unknown"):
+        super().__init__(id)
+        self.fabric = fabric
+    
+    def __str__(self):
+        first_message = super().__str__()
+        second_message = f"It is made from {self.fabric} fabric."
+        return f"{first_message} {second_message}"
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,18 @@
-class Decor:
+from swap_meet.item import Item
+
+class Decor(Item):
     pass
+    def __init__(self, id=None, width=0, length=0):
+        super().__init__(id)
+        self.width = width
+        self.length = length
+
+def get_category(self):
+    return self.__class__.__name__
+
+
+def __str__(self):
+    type_and_id_msg = super().__str__()
+    width_and_length_msg = f"It takes up a {self.width} by {self.length} sized space."
+    return f"{type_and_id_msg}{width_and_length_msg}"
+    
