496be4ea3388982e6d093681ead28c2ead9234c4
Say Ryder <lindz.ryder@gmail.com>
2023-04-05 17:06:50 -0400

    not this save

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -6,9 +6,9 @@ class Item:
             self.id = uuid.uuid4().int
         else:
             self.id = id
+            
     def __str__(self):
-        item_id = str(self.id)
-        return f'An object of type {self.__class__.__name__} with id {item_id}.'
+        return f'An object of type {self.__class__.__name__} with id {self.id}.'
 
     def get_category(self):
         return self.__class__.__name__
