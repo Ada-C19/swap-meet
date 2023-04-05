e6cd8c2606cce16008d5a8c20d0483a1d6c713d3
Lily <aulily09@gmail.com>
2023-04-05 11:09:34 -0700

    Removed helper function

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -23,18 +23,16 @@ class Vendor:
                 return item
             
         return None
-    
-    def swap_items_helper(self, other_vendor, my_item, their_item):
-        self.inventory.remove(my_item)
-        other_vendor.inventory.append(my_item)
-        other_vendor.inventory.remove(their_item)
-        self.inventory.append(their_item)
 
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
         
-        self.swap_items_helper(other_vendor, my_item, their_item)
+        self.inventory.remove(my_item)
+        other_vendor.inventory.append(my_item)
+        other_vendor.inventory.remove(their_item)
+        self.inventory.append(their_item)
+        
         return True
     
     def swap_first_item(self, other_vendor):
