c6f360a91d278ccbedce358eaf0701315bee03ff
Yulia S <startyupp@gmail.com>
2023-04-05 00:08:46 -0700

    Completed vendor.py for wave 1, completed unit test for wave 1. All passsed. Working in progress on item.py. Commented out skip tests parts on tests wave 2.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,19 @@
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
-        
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
     def remove(self, item):
         if not self.inventory or item not in self.inventory:
             return False
+        self.inventory.remove(item)
         return item
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
