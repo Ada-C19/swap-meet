1c861cfd6a082c4c7d8b38e5cefc52bcff7467cf
Whitney <whitneyshake@gmail.com>
2023-04-04 17:16:25 -0700

    Wave 01 Passing One Test To Write

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,24 @@
+
+
 class Vendor:
-    pass
+    
+    def __init__(self, inventory = None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def vendor_has_inventory(self):
+        if len(self.inventory) > 0:
+            return self.inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item
