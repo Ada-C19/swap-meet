c31b1ba8c56892cbe9437d3dcce0175f81ef3f01
Angelica <angelica.yee57@gmail.com>
2023-04-04 19:51:19 -0500

    clothing at w5 passed, missing: condition_description function to align with other classes

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,18 @@
+import uuid
+
 class Clothing:
-    pass
+    
+    def __init__(self, id = None, fabric = "Unknown", condition = 0):
+        id = uuid.uuid4().int if id is None else id
+        self.id = id
+        self.fabric = fabric
+
+    def get_category(self):
+        return self.__class__.__name__
+    
+    def __repr__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+    
+    def condition_description(self):
+        descriptions = ["heavily used", "fair","good","like new","never worn","new with tag"]
+        return descriptions[self.condition]
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,15 +1,20 @@
 import uuid
 
 class Item:
-    def __init__(self, id = None):
+    def __init__(self, id = None, condition = 0):
         id = uuid.uuid4().int if id is None else id
         self.id = id
+        self.condition = condition
 
     def __repr__(self):
         return f"An object of type Item with id {self.id}."
 
     def get_category(self):
         return self.__class__.__name__
+    
+    def condition_description(self):
+        descriptions = ["heavily used", "fair","good","like new","never worn","new with tag"]
+        return descriptions[self.condition]
     
 
 
