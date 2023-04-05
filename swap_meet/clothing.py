c9f0fdfa14e343ef209c4a3aea67322cd67993e4
Arshdeep Chauhan <avc.swe17@gmail.com>
2023-04-05 14:29:09 -0700

    Modified files for inheritance

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,13 +3,8 @@ from swap_meet.item import Item
 
 class Clothing(Item):
     def __init__(self, id= None, fabric=None, condition= None):
-        # super().__init___(id, condition)
+        super().__init__(id, condition)
         self.fabric = "Unknown" if fabric is None else fabric
-    
-        random_id = uuid.uuid4()
-        self.id = int(random_id) if id is None else id
-
-        self.condition = 0 if condition is None else condition
 
     def __str__(self):
         return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,9 +2,9 @@ import uuid
 from swap_meet.item import Item
 class Electronics(Item):
     def __init__(self,id=None, type=None, condition=None):
-        random_id = uuid.uuid4()
-        self.id = int(random_id) if id is None else id
+        super().__init__(id, condition)
         self.type = "Unknown" if type is None else type
-        self.condition = 0 if condition is None else condition
+
+
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
