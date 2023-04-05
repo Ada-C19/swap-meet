a6ecdd6cd94be1f7f6a477f9c7cc9bb80fa01aa3
Allie Soliz <solizale@gmail.com>
2023-04-05 12:35:23 -0400

    adds __str__ method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,6 @@
 import uuid
 
+
 class Item:
     def __init__(self, id=None):
         if id:
@@ -11,3 +12,6 @@ class Item:
     def get_category(self):
         class_name = Item()
         return class_name.__class__.__name__
+
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
