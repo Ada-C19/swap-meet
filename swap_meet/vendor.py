bb5b6e7fbf9191e214b0c63d67017b979717532d
chickenandegg <tei.villanueva@gmail.com>
2023-04-04 22:01:05 -0700

    initialize Vendor class and add method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,13 @@
 
 
-# testing git
 class Vendor:
-    pass
+    # here a new instance of Vendor can either have a list or None option as a value for inventory
+    def __init__(self, inventory=None):
+        if inventory:
+            self.inventory = inventory
+        else:
+            self.inventory = []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
