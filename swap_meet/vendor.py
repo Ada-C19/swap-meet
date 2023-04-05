407e609a8b9b9d156163c0342dd5c43a62ac2994
Jessica <Yiskah.Scientia@gmail.com>
2023-04-05 06:53:14 -0600

    Minor variable clean up

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -24,17 +24,15 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         if not (my_item in self.inventory and their_item in other_vendor.inventory):
             return False
-        else:
-            self.remove(my_item)
-            self.add(their_item)
-            other_vendor.remove(their_item)
-            other_vendor.add(my_item)
-            return True
+
+        self.remove(my_item).add(their_item)
+        other_vendor.remove(their_item).add(my_item)
+        return True
         
     def swap_first_item(self, other_vendor):
         if not (self.inventory and other_vendor.inventory):
             return False
-        else:
-            my_item = self.inventory[0]
-            their_item = other_vendor.inventory[0]
-            return self.swap_items(other_vendor, my_item, their_item)
+    
+        my_item = self.inventory[0]
+        their_item = other_vendor.inventory[0]
+        return self.swap_items(other_vendor, my_item, their_item)
