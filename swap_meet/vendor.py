980925f550cf2c1ccc587fbb5a2035c5e4ce5143
Jessica <Yiskah.Scientia@gmail.com>
2023-04-05 07:26:08 -0600

    Minor variable improvements

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,9 +1,9 @@
 class Vendor:
     def __init__(self, inventory = None):
         if inventory is None:
-            self.inventory  =  []
+            self.inventory = []
         else:
-            self.inventory  = inventory
+            self.inventory = inventory
             
     def add(self, item):
         self.inventory.append(item)
@@ -25,14 +25,17 @@ class Vendor:
         if not (my_item in self.inventory and their_item in other_vendor.inventory):
             return False
 
-        self.remove(my_item).add(their_item)
-        other_vendor.remove(their_item).add(my_item)
+        self.remove(my_item)
+        other_vendor.add(my_item)
+        other_vendor.remove(their_item)
+        self.add(their_item)
         return True
         
     def swap_first_item(self, other_vendor):
         if not (self.inventory and other_vendor.inventory):
             return False
-    
-        my_item = self.inventory[0]
-        their_item = other_vendor.inventory[0]
-        return self.swap_items(other_vendor, my_item, their_item)
+
+        # my_item = self.inventory[0]
+        # their_item = other_vendor.inventory[0]
+        # return self.swap_items(other_vendor, my_item, their_item)
+        return self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
