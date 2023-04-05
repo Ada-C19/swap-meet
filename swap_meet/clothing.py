e983a0d99cf1f2a6c7c0d97bcfdf605d78dfe075
Sophia Tran <sophiat8832@gmail.com>
2023-04-04 21:54:45 -0700

    fixed default arg id in constructor

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,9 +2,9 @@ from swap_meet.item import Item
 
 class Clothing(Item):
     def __init__(self, id=None, fabric="Unknown"):
-        super().__init__()
+        super().__init__(id)
         self.fabric = fabric
-
+    
     def get_category(self):
         return "Clothing"
     
