3ff831b38f26318423d2afb0216d2dbedf7c6bac
Abby Castillo <abby.machines@gmail.com>
2023-04-05 14:28:35 -0700

    successfully implemented Electronics subclass in electronics.py

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,15 @@
-class Electronics:
-    pass
+from .item import Item
+
+class Electronics(Item):
+    
+    def __init__(self, id=None, type="Unknown"):
+        super().__init__(id)
+        self.type = type
+    
+    def get_category(self):
+        return "Electronics"
+
+    def __str__(self):
+        line1 = super().__str__()
+        line2 = f"This is a {self.type} device."
+        return " ".join((line1, line2))
