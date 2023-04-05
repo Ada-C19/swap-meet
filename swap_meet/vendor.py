78b7c6cf395ed84db16b669d73f80acb2a395274
chickenandegg <tei.villanueva@gmail.com>
2023-04-04 22:11:34 -0700

    pass remove method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -13,3 +13,12 @@ class Vendor:
     def add(self, item):
         self.inventory.append(item)
         return item
+
+# if theres a match, item gets removed
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+
+        else:
+            return False
