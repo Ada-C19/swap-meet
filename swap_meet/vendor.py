6a976afbdba4ab9831e35cdf7e59010122bf8349
Yulia S <startyupp@gmail.com>
2023-04-05 11:18:49 -0700

    Wave 4 is complete. vendor.py is refactored. Tests passed.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -27,3 +27,8 @@ class Vendor:
         self.add(their_item)
         
         return True
+    
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        return self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
