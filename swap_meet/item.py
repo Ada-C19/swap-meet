250b5a7d06fd1c85432acb03bf75125bd9f96ef3
Yulia S <startyupp@gmail.com>
2023-04-05 00:32:58 -0700

    Fixed bug in init to get unique id for each object of class. Wave passed all tests.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,8 @@
 import uuid
 
 class Item:
-    def __init__(self, id=uuid.uuid4().int):
-        self.id = id
+    def __init__(self, id=None):
+        self.id = uuid.uuid4().int if id is None else id
     
     def get_category(self):
         return type(self).__name__
