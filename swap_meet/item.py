2b3139ba67d00b87ac00cd4a8f214918039293ed
Sarah Watkins <sarahwwatkins@gmail.com>
2023-04-05 11:57:27 -0400

    Completed first part of wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,7 +5,7 @@ class Item:
         self.id = uuid1().int if id is None else id
 
     def __str__(self):
-        return f"An object of type Item with id {self.id}."
+        return f"An object of type {self.get_category()} with id {self.id}."
 
     def get_category(self):
         return f"{self.__class__.__name__}"
