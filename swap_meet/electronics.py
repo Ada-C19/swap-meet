2674624d0e432c510230357877f162278071d8ab
Sophia Tran <sophiat8832@gmail.com>
2023-04-04 22:10:13 -0700

    implemented get_category for Electronics

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -4,3 +4,6 @@ class Electronics(Item):
     def __init__(self, id=None, type="Unknown"):
         super().__init__(id)
         self.type = type
+
+    def get_category(self):
+        return "Electronics"
