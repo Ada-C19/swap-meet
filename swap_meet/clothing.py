dc3287983bba1b29bf44a99e018d414541390960
Ana Villalobos <ana.villalobos309@gmail.com>
2023-04-04 19:56:23 -0700

    Creates Decor class. Decor is a child class and Item is the parent class. Passes all associated tests from test_wave_05

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,9 +1,9 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self,fabric = "Unknown", id = None):
-        self.fabric = fabric 
+    def __init__(self, id = None, fabric = "Unknown"):
         super().__init__(id)
+        self.fabric = fabric 
 
     def get_category(self):
         return "Clothing"
