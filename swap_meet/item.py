e7cb1bc390253e2b7b7742fef5faef5633eaa7a4
Jessica <Yiskah.Scientia@gmail.com>
2023-04-05 06:49:50 -0600

    Minor variable clean up

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -4,7 +4,7 @@ class Item:
     def __init__(self, id = None):
         
         if not id:
-            self.id = uuid.uuid4().int
+            self.id = uuid.uuid1().int
         else:
             self.id = id
 
@@ -14,18 +14,3 @@ class Item:
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}."
 
-# class Item
-# def __str__(self, item):
-# returns `"An object of type Item with id <id value>."`
-# For example, if we had an `Item` instance `item_a = Item(id=12345)`
-
-
-# class Vendor
-
-# def swap_items(self, other_vendor, my_item, their_item): -> my_item plans to give
-# remove my_item from self.vendor append to other_vendor
-# remove their_item from other_vendor append self.vendor[]
-# return True
-
-# if not my_item in vendor or not their_item in other_vendor:
-# return False
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -5,31 +5,30 @@ class Vendor:
         else:
             self.inventory  = inventory
             
-    def add(self, new_item):
-        self.inventory.append(new_item)
-        return new_item
+    def add(self, item):
+        self.inventory.append(item)
+        return item
         
-    def remove(self, remove_item):
-        if remove_item in self.inventory:
-            self.inventory.remove(remove_item)
-            return remove_item
-        else:
-            return False
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
         
-    def get_by_id(self, item_id):
+    def get_by_id(self, id):
         for item in self.inventory:
-            if item.id == item_id:
+            if item.id == id:
                 return item
         return None
     
     def swap_items(self, other_vendor, my_item, their_item):
-        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+        if not (my_item in self.inventory and their_item in other_vendor.inventory):
             return False
         else:
             self.remove(my_item)
-            other_vendor.add(my_item)
-            other_vendor.remove(their_item)
             self.add(their_item)
+            other_vendor.remove(their_item)
+            other_vendor.add(my_item)
             return True
         
     def swap_first_item(self, other_vendor):
