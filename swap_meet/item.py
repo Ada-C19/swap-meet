e987152912a45344ac0a48c37d8195287212aac3
Lily <aulily09@gmail.com>
2023-04-06 15:35:20 -0700

    Removed parenthesis in get_category()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -9,7 +9,7 @@ class Item:
         self.condition = condition
 
     def get_category(self):
-        return str(self.__class__.__name__)
+        return self.__class__.__name__
     
     def __str__(self):
         return f"An object of type Item with id {self.id}."
