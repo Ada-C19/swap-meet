1c935cffbf5722dde61a2216864df75e13cce600
Anh Huynh <avh2@uw.edu>
2023-04-06 12:14:28 -0700

    Modified vendor.py and test wave 1 and passed all tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+        
+    def add(self, item_add):
+        self.inventory.append(item_add)
+        return item_add
+    
+    def remove(self, item_remove):
+        if item_remove not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item_remove)
+            return item_remove
