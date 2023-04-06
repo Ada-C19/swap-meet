06bf75849149f489a8bb786100c86f7e0fa8a76b
Ariel Higuera <purpl3m00ns@gmail.com>
2023-04-05 17:56:30 -0700

    wave 5 clothing method complete and passing relevant tests

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,14 @@
+from uuid import uuid4
+
 class Clothing:
-    pass
+    def __init__(self, id=None, fabric="Unknown"):
+        self.fabric = fabric
+        self.id = id
+        if self.id is None:
+            self.id = uuid4().int
+
+    def __str__(self):
+        return f"An object of type {self.__class__.__name__} with id {self.id}. It is made from {self.fabric} fabric."
+
+    def get_category(self):
+        return self.__class__.__name__
