be3c86c0d0c4a770c3f2ca6f5921e978c3fb78da
Yulia S <startyupp@gmail.com>
2023-04-05 18:30:17 -0700

    Added doc string for n2w imported module and restyled condition_description method for better readability.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -13,23 +13,30 @@ class Item:
         return f'An object of type {self.get_category()} with id {self.id}.'
     
     def condition_description(self):
+        '''Imported n2w module: number to word, 
+        that converts given integer to the string/word version of it.'''
+        
         if isinstance(self.condition, int):
-            description = (f'{n2w.convert(self.condition).capitalize()} green bottles\n' 
+            description = (
+                f'{n2w.convert(self.condition).capitalize()} green bottles\n' 
         'Hanging on the wall\n'
         f'{n2w.convert(self.condition).capitalize()} green bottles\n'
         'Hanging on the wall\n'
         'And if one green bottle\n'
         'Should accidentally fall\n'
         f'There will be {n2w.convert(self.condition - 1)} green bottles\n'
-        'Hanging on the wall.')
+        'Hanging on the wall.'
+        )  
         else:
-            description = (f'{self.condition} green bottles\n' 
+            description = (
+                f'{self.condition} green bottles\n' 
         'Hanging on the wall\n'
         f'{self.condition} green bottles\n'
         'Hanging on the wall\n'
         'And if one green bottle\n'
         'Should accidentally fall\n'
         f'There will be {self.condition - 1} green bottles\n'
-        'Hanging on the wall.')
+        'Hanging on the wall.'
+        )
             
         return description
