1423bf27c68249a7f1ac81213a9049371ac9768f
Madison Jackson <madi.jackson29@gmail.com>
2023-04-06 15:15:35 -0700

    refactored swap-meet!

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,10 +1,11 @@
 import uuid
-class Decor:
+from swap_meet.item import Item
+
+class Decor(Item):
     def __init__(self,id = None, width = 0, length = 0, condition = 0):
-        self.id = id if id is not None else uuid.uuid4().int
+        super().__init__(id, condition)
         self.width = width
         self.length = length
-        self.condition = condition
 
     def get_category(self):
         return "Decor"
@@ -12,19 +13,7 @@ class Decor:
     def __str__(self):
         return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
     
-    def condition_description(self):
-        if self.condition == 0:
-            return "thats too low"
-        elif self.condition == 1:
-            return "that's low"
-        elif self.condition == 2:
-            return "almost half way"
-        elif self.condition == 3:
-            return "past half way"
-        elif self.condition == 4:
-            return "so close"
-        elif self.condition == 5:
-            return "perfect"
+
     
 
 
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,11 +1,12 @@
 import uuid
 from swap_meet.item import Item
 
-class Electronics:
-    def __init__(self,id = None, type = "Unknown", condition = 0):
-        self.id = id if id is not None else uuid.uuid4().int
+class Electronics(Item):
+    def __init__(self, id = None, type = "Unknown", condition = 0):
+        super().__init__(id, condition)
+        # self.id = id if id is not None else uuid.uuid4().int
         self.type = type
-        self.condition = condition
+        # self.condition = condition
 
     def get_category(self):
         return "Electronics"
@@ -13,16 +14,3 @@ class Electronics:
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
     
-    def condition_description(self):
-        if self.condition == 0:
-            return "thats too low"
-        elif self.condition == 1:
-            return "that's low"
-        elif self.condition == 2:
-            return "almost half way"
-        elif self.condition == 3:
-            return "past half way"
-        elif self.condition == 4:
-            return "so close"
-        elif self.condition == 5:
-            return "perfect"
