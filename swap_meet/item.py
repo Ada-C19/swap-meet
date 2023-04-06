325c736d544d47d4ee697d2ee6319cf4a9fd74ee
maggie weir <mlweir98@gmail.com>
2023-04-06 11:07:58 -0400

    finished wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,16 +1,29 @@
 from uuid import uuid4
 from uuid import getnode
-class Item:
-    def __init__(self, id = None):
-        self.id = id if id is not None else int(uuid4())
-        # self.id = int(uuid4())
 
-    def get_category(self, ):
+class Item:
+    def __init__(self, id = None, condition = 0, age = 0):
+        self.id = id if id is not None else uuid4().int
+        # self.id = int(uuid4())
+        self.condition = condition
+        self.conditions_dict = {
+            0: "There is a very evil, ancient spirit attached to this item",
+            1: "We unearthed this from a haunted gravesite",
+            2:"We unearthed this from an unhaunted gravesite",
+            3:"This item has never been buried in a gravesite",
+            4:"This item is used, but has never been haunted",
+            5:"This item is brand new and has had no opportunity to be touched by the spirits...yet"}
+        self.age = age
+
+    def get_category(self):
         return self.__class__.__name__
     
     def __str__(self):
         return f"An object of type Item with id {self.id}."
 
+    def condition_description(self):
+        return self.conditions_dict[self.condition]
+        
 # In Wave 2 we will create the `Item` class and the `Vendor` class' `get_by_id` method.
 
 # - There is a module (file) named `item.py` inside of the `swap_meet` package (folder)
