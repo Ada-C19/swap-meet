712d01aa1e5f5649784d0121c9e6c458992c48e5
Ariel Higuera <purpl3m00ns@gmail.com>
2023-04-05 18:02:11 -0700

    wave 5 decor method complete and passing relevant tests

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,15 @@
+from uuid import uuid4
+
 class Decor:
-    pass
+    def __init__(self, id=None, width=0, length=0):
+        self.id = id
+        if self.id is None:
+            self.id = uuid4().int
+        self.width = width
+        self.length = length
+
+    def __str__(self):
+        return f"An object of type {self.__class__.__name__} with id {self.id}. It takes up a {self.width} by {self.length} sized space."
+
+    def get_category(self):
+        return self.__class__.__name__
