5a2efe18029b8235d7bd5a5c0400be690792274b
Rainacam <rainacam12@gmail.com>
2023-04-06 11:28:31 -0400

    removed comments

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -8,23 +8,7 @@ class Decor(Item):
         # self.id = id
         self.width = width
         self.length = length
-        # self.condition = condition
-
-        # if id == None: 
-        #     self.id = uuid.uuid4().int
-        # else: 
-        #     self.id
-
-    # def get_category(self): 
-    #     return self.__class__.__name__
 
     def __str__(self):
         return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
 
-    # def condition_description(self): 
-    #     if self.condition <= 1: 
-    #         return "You probably want a glove for this one..."
-    #     elif self.condition <= 3: 
-    #         return "It's aight."
-    #     elif self.condition <= 5: 
-    #         return "Practically brand new, almost."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -7,24 +7,9 @@ class Electronics(Item):
         super().__init__(id, condition)
         # self.id = id
         self.type = type
-        # self.condition = condition
 
-        # if id == None: 
-        #     self.id = uuid.uuid4().int
-        # else: 
-        #     self.id
-        
-
-    # def get_category(self): 
-    #     return self.__class__.__name__
 
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
     
-    # def condition_description(self): 
-    #     if self.condition <= 1: 
-    #         return "You probably want a glove for this one..."
-    #     elif self.condition <= 3: 
-    #         return "It's aight."
-    #     elif self.condition <= 5: 
-    #         return "Practically brand new, almost."
+
