2a427c0c6ff8d213f619dbb707a2655d4be705e6
Stacey <stacey.l.joseph@gmail.com>
2023-04-06 17:30:11 -0400

    commented out W6 tests for last insertion method

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -86,7 +86,7 @@ def test_best_by_category_with_duplicates():
     assert best_item.get_category() == "Clothing"
     assert best_item.condition == pytest.approx(4.0)
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category():
     # Arrange
     # me
@@ -133,7 +133,7 @@ def test_swap_best_by_category():
     # ****** Completed Assert Portion of this test **********
     # *********************************************************************
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_reordered():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -176,17 +176,14 @@ def test_swap_best_by_category_reordered():
     # *********************************************************************
     # ****** Completed Assert Portion of this test **********
     # *********************************************************************
-<<<<<<< HEAD
     # Assertions should check:
     # - That result is truthy
     # - That tai and jesse's inventories are the correct length
     # - That all the correct items are in tai and jesse's inventories, and that the items that were swapped are not there
 
-@pytest.mark.skip
-=======
+
     
 # @pytest.mark.skip
->>>>>>> 855cf4f75a59a9c48a63aad318c0755a06d075a8
 def test_swap_best_by_category_no_inventory_is_false():
     tai = Vendor(
         inventory=[]
@@ -212,7 +209,7 @@ def test_swap_best_by_category_no_inventory_is_false():
     assert item_b in jesse.inventory
     assert item_c in jesse.inventory
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_no_other_inventory_is_false():
     item_a = Clothing(condition=2.0)
     item_b = Decor(condition=4.0)
@@ -238,7 +235,7 @@ def test_swap_best_by_category_no_other_inventory_is_false():
     assert item_b in tai.inventory
     assert item_c in tai.inventory
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_no_match_is_false():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -277,7 +274,7 @@ def test_swap_best_by_category_no_match_is_false():
     # ****** Completed Assert Portion of this test **********
     # *********************************************************************
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_no_other_match_is_false():
     # Arrange
     item_a = Decor(condition=2.0)
