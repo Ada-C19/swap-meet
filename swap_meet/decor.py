9acf4df3388a43920b5aeec16d7e87713d400c87
Abby <argoodm@gmail.com>
2023-04-06 11:54:05 -0400

    refactor decor and electric using super

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,14 +1,13 @@
-import uuid
+
 from swap_meet.item import Item
 
 class Decor(Item):
     
 
     def __init__(self, id=None, width=0, length=0, condition= 0.0):
-        self.id = id if id is not None else int(uuid.uuid4())
+        super().__init__(id, condition)
         self.width = width 
         self.length = length
-        self.condition = condition
     
     def __str__(self):
         return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,12 +1,10 @@
-import uuid
 from swap_meet.item import Item
     
 class Electronics(Item):
 
     def __init__(self, id=None, type="Unknown", condition= 0.0):
-        self.id = id if id is not None else int(uuid.uuid4())
+        super().__init__(id, condition)
         self.type = type
-        self.condition = condition
 
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
