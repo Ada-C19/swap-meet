35915546c1c67fad10ca7a9b5c7f5660a7c2395f
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-04-05 18:51:57 -0700

    added the stringify instance to item.py

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -10,7 +10,13 @@ class Item:
 
 
     def get_category(self):
-        return self.__class__.__name__
+        return (self.__class__.__name__)
+    
+    print("_____________")
+
+    def __str__(self):
+        
+        return f'An object of type {self.get_category()} with id {self.id}.'
 
 
 
