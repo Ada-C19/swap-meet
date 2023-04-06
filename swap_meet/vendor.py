310ba88dd94a4cf873ea512a0ccda362e9c0151b
Anh Huynh <avh2@uw.edu>
2023-04-06 16:12:08 -0700

    Modified vendor.py and test wave 4 files and passed all tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -26,4 +26,25 @@ class Vendor:
         return None
 
             
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        else:
+            self.inventory.remove(my_item)
+            other_vendor.inventory.append(my_item)
+            other_vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            return True
+    
+    def swap_first_item(self, other_vendor):
+        if len(self.inventory) == 0 or len(other_vendor.inventory) == 0:
+            return False
+        else:
+            user_first_item = self.inventory[0]
+            friend_first_item = other_vendor.inventory[0]
+            self.inventory.remove(user_first_item)
+            other_vendor.inventory.remove(friend_first_item)
+            other_vendor.inventory.append(user_first_item)
+            self.inventory.append(friend_first_item)
+            return True
         
