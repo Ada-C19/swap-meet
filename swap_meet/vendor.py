b61e0cbc090eada5b4481c0d991cb93d825c050b
Virginia Ramos <virginia.rare@gmail.com>
2023-04-06 14:11:30 -0500

    Swap_by_newest with lambda

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -70,33 +70,40 @@ class Vendor:
             self.swap_items(other_vendor, my_best_item, their_best_item)
             return True
 
-    def get_by_age(self,age):
-        age_list = []
-        for item in self.inventory:
-            if item.get_age() == age:
-                age_list.append(item)
-        return age_list 
+    # def get_by_age(self,age):
+    #     age_list = []
+    #     for item in self.inventory:
+    #         if item.get_age() == age:
+    #             age_list.append(item)
+    #     return age_list 
         
     
-    def get_newest_item(self,age):
-        items = self.get_by_age(age)
-        newest_item = 0
-        for item in items:
-            if item.age < newest_item:
-                newest_item = item 
-        return newest_item
+    # def get_newest_item(self,age):
+    #     items = self.get_by_age(age)
+    #     newest_item = 0
+    #     for item in items:
+    #         if item.age < newest_item:
+    #             newest_item = item 
+    #     return newest_item
         
     
-    def swap_by_newest(self, other_vendor, my_item, their_item):
-        my_newest_item = self.get_newest_item(their_item)
-        their_newest_item = other_vendor.get_newest_item(my_item)
+    # def swap_by_newest(self, other_vendor, my_item, their_item):
+    #     my_newest_item = self.get_newest_item(their_item)
+    #     their_newest_item = other_vendor.get_newest_item(my_item)
 
-        if not my_newest_item or not their_newest_item:
+    #     if not my_newest_item or not their_newest_item:
+    #         return False
+    #     else: 
+    #         self.swap_items(other_vendor, my_newest_item, their_newest_item)
+    #         return True
+    #     pass
+
+    def swap_by_newest(self,other_vendor):
+        my_newest_item = min(self.inventory, key= lambda item: item.age, default=None)
+        their_newest_item = min(other_vendor.inventory, key=lambda item: item.age, default=None)
+
+        if my_newest_item and their_newest_item:
+            return self.swap_items(other_vendor, my_newest_item, their_newest_item)
+        
+        else:
             return False
-        else: 
-            self.swap_items(other_vendor, my_newest_item, their_newest_item)
-            return True
-        pass
-    #to check in both inventories for "modern" items, swap first instance of modern item)
-    #if age of both items is < 10: use swap items method!
-    #OR if items == "modern", use swap items method
--- tests/unit_tests/test_wave_bonus.py
+++ tests/unit_tests/test_wave_bonus.py
@@ -23,3 +23,26 @@ def test_get_by_age_success():
 
     #assert
     assert len(items) == 1
+
+def test_swap_by_newest():
+    #Arrange
+    item_a= Item(age=5)
+    item_b= Item(age=6)
+    item_c= Item(age=7)
+    tai = Vendor(inventory=[item_a, item_b, item_c])
+    
+    item_d= Item(age=8)
+    item_e= Item(age=9)
+    item_f= Item(age=4)
+    jesse = Vendor(inventory=[item_d, item_e, item_f])
+
+    #Act
+    result = jesse.swap_by_newest(other_vendor=tai)
+
+    #Assert
+    assert result is not False 
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+    assert item_a in jesse.inventory 
+    assert item_f in tai.inventory
+
