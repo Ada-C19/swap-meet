043627683638acf0142dc846da697ff7bcc42337
Ariel Higuera <purpl3m00ns@gmail.com>
2023-04-05 18:04:51 -0700

    wave 5 electronics method complete and passing relevant tests

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,14 @@
+from uuid import uuid4
+
 class Electronics:
-    pass
+    def __init__(self, id=None, type="Unknown"):
+        self.type = type
+        self.id = id
+        if self.id is None:
+            self.id = uuid4().int
+
+    def __str__(self):
+        return f"An object of type {self.__class__.__name__} with id {self.id}. This is a {self.type} device."
+
+    def get_category(self):
+        return self.__class__.__name__
