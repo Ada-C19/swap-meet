391ee95fb1926a687f2c41c2ceb6091d5efafdd4
Doris Lin <lmh7026@gmail.com>
2023-04-06 16:51:32 -0700

    Clean up spacing between methods in Clothin

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,15 +3,18 @@ from .item import Item
 class Clothing(Item):
     
     def __init__(self, age=0, id=None, condition=0, fabric="Unknown"):
-        super().__init__(age, id, condition,)
+        super().__init__(age, id, condition)
         self.fabric = fabric
+    
     
     def get_category(self):
         return "Clothing"
     
+
     def __str__(self):
         line1 = super().__str__()
         line2 = f"It is made from {self.fabric} fabric."
+
         return " ".join((line1, line2))
 
         
