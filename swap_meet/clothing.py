15a8787fc9ce1b488193e5a6341bca4d1a85a692
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-04-06 13:35:02 -0700

    clothing function working

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -5,7 +5,12 @@ class Clothing:
     def __init__(self,id = None, fabric = "Unknown"):
         if not id:
             id = int(uuid.uuid4())
-            
+        self.id = id
+
         self.fabric = fabric
 
-        pass
+    def get_category(self):
+        return (self.__class__.__name__)
+    
+    def __str__(self):
+        return( f'An object of type {self.get_category()} with id {self.id}. It is made from {self.fabric} fabric.')
