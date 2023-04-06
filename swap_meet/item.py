39cb9781728d837353fdfe119c2e1e49089b30e6
Yulia S <startyupp@gmail.com>
2023-04-05 18:24:37 -0700

    Added condition attribute to class Item and defined condition_description method. Commented out pytest.skip.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,35 @@
 import uuid
+import n2w
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         self.id = uuid.uuid4().int if id is None else id
+        self.condition = condition
     
     def get_category(self):
         return type(self).__name__
     
     def __str__(self):
         return f'An object of type {self.get_category()} with id {self.id}.'
+    
+    def condition_description(self):
+        if isinstance(self.condition, int):
+            description = (f'{n2w.convert(self.condition).capitalize()} green bottles\n' 
+        'Hanging on the wall\n'
+        f'{n2w.convert(self.condition).capitalize()} green bottles\n'
+        'Hanging on the wall\n'
+        'And if one green bottle\n'
+        'Should accidentally fall\n'
+        f'There will be {n2w.convert(self.condition - 1)} green bottles\n'
+        'Hanging on the wall.')
+        else:
+            description = (f'{self.condition} green bottles\n' 
+        'Hanging on the wall\n'
+        f'{self.condition} green bottles\n'
+        'Hanging on the wall\n'
+        'And if one green bottle\n'
+        'Should accidentally fall\n'
+        f'There will be {self.condition - 1} green bottles\n'
+        'Hanging on the wall.')
+            
+        return description
