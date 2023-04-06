ffe1a2254e430eded1b01674ecde8f976fd7feb3
Sarah Watkins <sarahwwatkins@gmail.com>
2023-04-06 13:33:00 -0400

    Completed wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,9 +1,9 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, id=None, fabric="Unknown"):
+    def __init__(self, id=None, condition=0, fabric="Unknown"):
         self.fabric = fabric
-        super().__init__(id)
+        super().__init__(id, condition)
 
     def __str__(self):
         return f"{super().__str__()} It is made from {self.fabric} fabric."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,9 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, id=None, condition=0, type="Unknown"):
+        self.type = type
+        super().__init__(id, condition)
+
+    def __str__(self):
+        return f"{super().__str__()} This is a {self.type} device."
