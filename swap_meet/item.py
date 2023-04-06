2a3d9b38ecb641306123f2cd43a0ac5fbef3ef4e
Niambi <niambiada19@gmail.com>
2023-04-05 22:13:09 -0700

    Added sudo code to item.py, party.py, & ran tests in test_wave_01_02_03.py to check for failures

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,28 @@
+from uuid import (package)
 class Item:
-    pass
+    def __init__(self, id,):
+        self.id = #unique int = unique int
+        self.
+        self.
+
+    def 
+
+    def
+
+
+# -------- WAVE 2 -------------
+#Each item in item class should have an attribute named id
+#This attribute is a unique int
+#use python's uuid package to help generate numbers without duplicates
+#find which one of uuid's functions creates large unique numbers used as identifiers
+# these functions will return uuid objects **(not int)** 
+# uuid objects have an attribute int which allows us ti access their value as an int
+#when we intitialize and instance of items, we can optionally pass in an int with the keyword argument id to manually set the Items's id
+#Each item will have a function mamed get_category
+#Funciton will return a string holding the name of the class
+#instances of vendor have an instance method get_by_id
+
+#---------- WAVE 3 -------------
+
+# Create a method to stringify an item using str() using method swap_items
+#
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,34 @@
 class Vendor:
-    pass
+    def __init__(self, id, inventory, add_item, remove_item):
+        self.id = id
+        self.inventory = []
+        self.add_item = add_item
+        self.remove_item = remove_item
+    def add_item():
+        return #item that was added
+    
+    def remove_item(): 
+        return #item that was removed
+    
+
+
+
+
+    
+    
+#each vendor will have an attribute named :inventory (an empty list)
+#when we instantiate the instance of vendor, we can pass in a list with the argument inventory
+#every instance of vendor has an instance menthod .add()
+#.add() take is 1 item and adds it to inventory
+#returns the item that was added
+# every instance of vendor has a instance method .remove()
+# .remove() takes in one item and removes matching item from inventory
+#returns the item that was removed
+#If no matching item found in inventory it returns False
+
+# ---------------- WAVE 2 --------------------
+#create get_by_id method
+#instances of vendor have an instance method named get_by_id
+#takes in one argument (int) which reps items id 
+#this method returns item with matching id from inventory
+#if there is no item with matching id, the method should return None
