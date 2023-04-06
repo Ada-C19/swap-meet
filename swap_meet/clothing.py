0d74cf5fd56d96259f771f3cbf02b95ec6671078
Janice <janicenguyenn@gmail.com>
2023-04-06 14:31:08 -0700

    completed init, get_category and stringify method forClothing class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,17 @@
+import uuid
 class Clothing:
-    pass
+    def __init__(self,id=None, fabric="Unknown", condition=0):
+        if id is None:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+        self.fabric = fabric
+        self.condition = condition
+
+
+    def get_category(self):
+        return self.__class__.__name__
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+    
