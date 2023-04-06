3cd4bf8499cef4286ce726a25e9a1cdba55c363c
maggie weir <mlweir98@gmail.com>
2023-04-06 12:02:54 -0400

    refactoring

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,6 +22,7 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         if their_item not in other_vendor.inventory or my_item not in self.inventory:
             return False
+        
         else:
             other_vendor.remove(their_item)
             self.add(their_item)
@@ -33,6 +34,7 @@ class Vendor:
     def swap_first_item(self,other_vendor):
         if self.inventory == [] or other_vendor.inventory == []:
             return False
+        
         else:
             self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
         
@@ -48,11 +50,15 @@ class Vendor:
 
     def get_best_by_category(self, category):
         condition_value_dict = {}
+
         if self.get_by_category(category):
             for item in self.get_by_category(category):
                 condition_value_dict[item] = item.condition
+            
             best_item = max(condition_value_dict, key = condition_value_dict.get)
+            
             return best_item
+        
         else:
             return None
     
@@ -60,13 +66,17 @@ class Vendor:
         if self.get_best_by_category(their_priority) and other_vendor.get_best_by_category(my_priority):
             self.swap_items(other_vendor, self.get_best_by_category(their_priority), other_vendor.get_best_by_category(my_priority))
             return True
+        
         return False
 
     def get_newest_item(self):
         age_dict = {}
+
         for item in self.inventory:
             age_dict[item] = item.age
+
         newest_item = min(age_dict, key = age_dict.get)
+
         return newest_item
         
     def swap_by_newest(self, other_vendor):
