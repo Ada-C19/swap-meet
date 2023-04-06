92f35677da9a4ab6ca8983f5e156762af19600ef
Yulia S <startyupp@gmail.com>
2023-04-05 21:13:16 -0700

    Created instance method for Wave 6: get_by_category, get_best_by_category, and swap_best_by_category. Passed the tests.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from .item import Item
+
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
@@ -21,14 +23,44 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
+        
         self.remove(my_item)
         other_vendor.add(my_item)
         other_vendor.remove(their_item)
         self.add(their_item)
-        
         return True
     
     def swap_first_item(self, other_vendor):
         if not self.inventory or not other_vendor.inventory:
             return False
         return self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
+    
+    def get_by_category(self, category):
+        same_category_items = []
+        
+        for item in self.inventory:
+            if item.get_category() == category:
+                same_category_items.append(item)
+        return same_category_items
+    
+    def get_best_by_category(self, category):
+        items = self.get_by_category(category)
+
+        if not items:
+            return None
+        
+        best = items[0]
+        for item in items:
+            if item.condition > best.condition:
+                best = item
+        return best
+    
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        vendor_item_self_wants =  other_vendor.get_best_by_category(my_priority) 
+        self_item_vendor_wants = self.get_best_by_category(their_priority)
+        
+        if not vendor_item_self_wants or not self_item_vendor_wants:
+            return False 
+        
+        self.swap_items(other_vendor, self_item_vendor_wants, vendor_item_self_wants)
+        return True
