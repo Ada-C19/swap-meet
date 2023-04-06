c6fe65f0fd58e59a5272a82a6fd1ce1989b6cf41
Aiyana Ball <ballaiyana@gmail.com>
2023-04-05 22:06:41 -0700

    updated Item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,14 +2,20 @@ import uuid
 
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         if id is None:
             self.id = uuid.uuid4().int
         else:
             self.id = id
 
+        self.condition = condition
+
     def get_category(self):
         return "Item"
 
     def __str__(self):
-        return f"An object of type Item with id {str(self.id)}."
+        return f"An object of type Item with id {self.id}."
+    
+    def condition_description(self):
+        condition = ["0/5", "1/5", "2/5", "3/5", "4/5", "5/5"]
+        return condition[self.condition]
