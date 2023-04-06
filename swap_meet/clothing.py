ee50237de8d9acd52a4348cef3507516ebbda6be
Luwam Ghebremicael <luwammikael@gmail.com>
2023-04-05 18:21:15 -0700

    wave 5 passing

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,27 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, id=None, condition=0, fabric = "Unknown"):
+        super().__init__(id, condition)
+        self.fabric = fabric
+
+    def get_category(self):
+        return "Clothing"
+        
+    
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+
+
+#     - This attribute describes what fabric the clothing is made from; 
+#     some example values might be `"Striped"`, `"Cotton"`, or `"Floral"`
+#     - When we instantiate an instance of `Clothing`, 
+
+#     we can optionally pass in a string with the keyword argument `fabric`
+
+
+#   - Has a stringify method that returns `"An object of type
+# Clothing with id <id value>. It is made from <fabric value> fabric."`
+#     - For example, if we had a `Clothing` instance with an `id` of `123435` 
+#     and a `fabric` attribute that holds `"Wool"`, its stringify method should
+#     return `"An object of type Clothing with id 12345. It is made from Wool fabric."`
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -20,3 +20,26 @@ class Item:
     def __str__(self):
         return f"An object of type {self.__class__.__name__} with id {self.id}."
     
+    def __init__(self, id = None, condition=0):
+        self.condition = condition
+        if id is None:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+
+    def get_category(self):
+        return "Item"
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "This item is in terrible condition"
+        elif self.condition == 1:
+            return "This item is in very bad condition"
+        elif self.condition == 2:
+            return "This item is in bad condition"
+        elif self.condition == 3:
+            return "This item is in good condition"
+        elif self.condition == 4:
+            return "This item is in very good condition"
+        elif self.condition == 5:
+            return "This item is in excellent condition"
