49087212e5fcfd4efe19770348a2d786ac393699
Divya <divya.naidoo5@gmail.com>
2023-04-06 13:40:04 -0700

    completed wave 5 and got tests to pass

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,12 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, id=None, fabric="Unknown", condition=0.0):
+        super().__init__(id)
+        self.fabric = fabric 
+        self.condition = condition
+    
+    def __str__(self):
+        category_message = super().__str__()
+        return f"{category_message} It is made from {self.fabric} fabric."
+        
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,14 +1,28 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0.0):
         if id is None:
             id = uuid.uuid4().int
         self.id = id
+        self.condition = condition
     
     def get_category(self):
         return self.__class__.__name__
     
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}."
-        
+    
+    def condition_description(self):
+        if self.condition == 0.0:
+            return "Mint condition"
+        elif self.condition == 1.0:
+            return "Good condition"
+        elif self.condition == 2.0:
+            return "Lightly used"
+        elif self.condition == 3.0:
+            return "Fair"
+        elif self.condition == 4.0:
+            return "Used"
+        else:
+            return "Don't bother"
