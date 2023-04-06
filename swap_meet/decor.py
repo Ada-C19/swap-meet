e1a2bf56ef8d905736fe980808bc66153da7f753
Elaine Watkins <ewatki.ga@gmail.com>
2023-04-06 11:26:01 -0400

    added super()

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,27 +1,30 @@
-import uuid 
+# import uuid 
+from swap_meet.item import Item
 
-class Decor:
+
+class Decor(Item):
     def __init__(self, id = None, width = 0, length = 0, condition = 0): 
-        self.id = id
+        super().__init__(id, condition)
+        # self.id = id
         self.width = width
         self.length = length
-        self.condition = condition
+        # self.condition = condition
 
-        if id == None: 
-            self.id = uuid.uuid4().int
-        else: 
-            self.id
+        # if id == None: 
+        #     self.id = uuid.uuid4().int
+        # else: 
+        #     self.id
 
-    def get_category(self): 
-        return self.__class__.__name__
+    # def get_category(self): 
+    #     return self.__class__.__name__
 
     def __str__(self):
         return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
 
-    def condition_description(self): 
-        if self.condition <= 1: 
-            return "You probably want a glove for this one..."
-        elif self.condition <= 3: 
-            return "It's aight."
-        elif self.condition <= 5: 
-            return "Practically brand new, almost."
+    # def condition_description(self): 
+    #     if self.condition <= 1: 
+    #         return "You probably want a glove for this one..."
+    #     elif self.condition <= 3: 
+    #         return "It's aight."
+    #     elif self.condition <= 5: 
+    #         return "Practically brand new, almost."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,27 +1,30 @@
-import uuid 
+# import uuid 
+from swap_meet.item import Item
 
-class Electronics:
+
+class Electronics(Item):
     def __init__(self, id = None, type = "Unknown", condition = 0): 
-        self.id = id
+        super().__init__(id, condition)
+        # self.id = id
         self.type = type
-        self.condition = condition
+        # self.condition = condition
 
-        if id == None: 
-            self.id = uuid.uuid4().int
-        else: 
-            self.id
+        # if id == None: 
+        #     self.id = uuid.uuid4().int
+        # else: 
+        #     self.id
         
 
-    def get_category(self): 
-        return self.__class__.__name__
+    # def get_category(self): 
+    #     return self.__class__.__name__
 
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
     
-    def condition_description(self): 
-        if self.condition <= 1: 
-            return "You probably want a glove for this one..."
-        elif self.condition <= 3: 
-            return "It's aight."
-        elif self.condition <= 5: 
-            return "Practically brand new, almost."
+    # def condition_description(self): 
+    #     if self.condition <= 1: 
+    #         return "You probably want a glove for this one..."
+    #     elif self.condition <= 3: 
+    #         return "It's aight."
+    #     elif self.condition <= 5: 
+    #         return "Practically brand new, almost."
