791db8a13fe918553e3923b2b820a7fd374efb33
Samantha Hurtado <samanthahurtado@samanthas-mbp.lan>
2023-04-05 20:11:38 -0700

    fixed best by category function

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -29,16 +29,26 @@ class Vendor:
         
         return items_in_category
     
-    # def get_best_by_category(self, category):
-
+    def get_best_by_category(self, category):
+        best_condition_score = 0.0
+        best_item = None
+        for item in self.inventory:
+            if item.get_category() == category:
+                if item.condition > best_condition_score:
+                    best_item = item
+                    best_condition_score = item.condition
+        
+        return best_item
+            
+         
     
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False 
-        self.inventory.remove(my_item)
-        other_vendor.inventory.append(my_item)
-        other_vendor.inventory.remove(their_item)
         self.inventory.append(their_item)
+        other_vendor.inventory.append(my_item)
+        self.inventory.remove(my_item)
+        other_vendor.inventory.remove(their_item)
         return True 
 
     def swap_first_item(self, other_vendor):
