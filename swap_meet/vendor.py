6dc42fb3017b0dc3d5f186fccc4ff19116f67cd1
Diana <diana38974@gmail.com>
2023-04-06 10:53:49 -0400

    Swap_first_item function implementation for wave 4.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -38,3 +38,18 @@ class Vendor:
         other_vendor.remove(their_item)
 
         return True
+    
+    def swap_first_item(self, other_vendor):
+
+        if self.inventory == [] or other_vendor.inventory == []:
+            return False
+        
+        their_item = other_vendor.inventory[0]
+        my_item = self.inventory[0]
+        a = self.swap_items(other_vendor, my_item, their_item)
+        
+        return a
+        
+
+
+
