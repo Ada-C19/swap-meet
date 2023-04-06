17f61d7b9fce77f4f284b85eadd0342c65208d2c
Ericka <erickamoreno529@gmail.com>
2023-04-06 14:43:11 -0700

    wave 5 coded, condition_description implemented

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,20 +1,14 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, id=None, fabric="Unknown"):
-        super().__init__(id)
+    def __init__(self, id=None, fabric="Unknown", condition=0):
+        super().__init__(id, condition)
         self.fabric = fabric 
-        
 
     def get_category(self):
         return "Clothing"
     
     def __str__(self):
         return f"{super().__str__()} It is made from {self.fabric} fabric."
-#         return f"An object of type Clothing with id {self.__annotations__}"
     
-# "An object of type Clothing with id <id value>. It is made from <fabric value> fabric."
 
-
-# try out __name__ 
-# type(self).__name__
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,13 +1,30 @@
 import uuid
 
 class Item:
-    def __init__(self, id= None):
+    def __init__(self, id= None, condition=0):
         if id is not None:
             self.id = uuid.uuid4()
         self.id = id or int(uuid.uuid4())
+        self.condition = condition
         
     def get_category(self):
         return "Item"
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "very bad"
+        elif self.condition == 1:
+            return "pretty bad"
+        elif self.condition == 2:
+            return "ehhhh, it'll do"
+        elif self.condition == 3:
+            return "pretty good"
+        elif self.condition == 4:
+            return "very good"
+        elif self.condition == 5:
+            return "brand new"
+
+        
     
 
     def __str__(self):
