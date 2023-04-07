932273190253ffaedd044df82fc9ebf9b4cebc17
Charlie <charliethemushi@gmail.com>
2023-04-06 18:20:06 -0700

    added assertions for swap best by cat, swap best by cat reordered

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -1,4 +1,5 @@
 import pytest
+import unittest
 from swap_meet.item import Item
 from swap_meet.vendor import Vendor
 from swap_meet.clothing import Clothing
@@ -84,7 +85,7 @@ def test_best_by_category_with_duplicates():
     assert best_item.get_category() == "Clothing"
     assert best_item.condition == pytest.approx(4.0)
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_swap_best_by_category():
     # Arrange
     # me
@@ -110,7 +111,11 @@ def test_swap_best_by_category():
         their_priority="Decor"
     )
 
-    raise Exception("Complete this test according to comments below.")
+    assert result is True
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+    assert tai.inventory == tai.inventory
+    assert jesse.inventory == jesse.inventory
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
@@ -143,7 +148,13 @@ def test_swap_best_by_category_reordered():
         their_priority="Decor"
     )
 
-    raise Exception("Complete this test according to comments below.")
+    assert result is True
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+    assert tai.inventory == [item_c, item_b, item_a]
+    assert jesse.inventory == [item_f, item_e, item_d]
+    assert [item_f, item_e, item_d] != tai.inventory 
+    assert [item_c, item_b, item_a] != jesse.inventory
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
@@ -152,7 +163,7 @@ def test_swap_best_by_category_reordered():
     # - That tai and jesse's inventories are the correct length
     # - That all the correct items are in tai and jesse's inventories, and that the items that were swapped are not there
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_swap_best_by_category_no_inventory_is_false():
     tai = Vendor(
         inventory=[]
@@ -178,7 +189,7 @@ def test_swap_best_by_category_no_inventory_is_false():
     assert item_b in jesse.inventory
     assert item_c in jesse.inventory
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_swap_best_by_category_no_other_inventory_is_false():
     item_a = Clothing(condition=2.0)
     item_b = Decor(condition=4.0)
