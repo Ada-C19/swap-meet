522b6707d7ab2ca4f021ca2144ec65bdf1fae0b2
Valerie Valentin <valerie.valentine93@gmail.com>
2023-04-06 20:11:40 -0700

    Finished Wave 6 Complete

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,3 @@
-from swap_meet.item import Item
-from swap_meet.decor import Decor
-from swap_meet.electronics import Electronics
-from swap_meet.clothing import Clothing
 
 class Vendor:
     def __init__(self, inventory=None):
@@ -26,24 +22,17 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         if their_item not in other_vendor.inventory or my_item not in self.inventory:
             return False
-        # self.inventory.remove(my_item)
-        # self.inventory.append(their_item)
-        # other_vendor.inventory.remove(their_item)
-        # other_vendor.inventory.append(my_item)
-        # return True
-
-        self.inventory.append(other_vendor.inventory.pop(other_vendor.inventory.index(their_item)))
-        other_vendor.inventory.append(self.inventory.pop(self.inventory.index(my_item)))
+        
+        self.inventory.remove(my_item)
+        self.inventory.append(their_item)
+        other_vendor.inventory.remove(their_item)
+        other_vendor.inventory.append(my_item)
         return True
+
         
     def swap_first_item(self, other_vendor):
         if not self.inventory or not other_vendor.inventory:
             return False
-
-        my_first_item = self.inventory[0]
-        # their_first_item = other_vendor.inventory[0]
-
-        # self.swap_items(other_vendor, my_first_item, their_first_item)
 
         return self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
     
@@ -54,32 +43,6 @@ class Vendor:
                 current_stock.append(item)
         return current_stock
 
-    def get_best_by_category(self, string):
-        best_item = None
-        highest_condition = 0
-        for item in self.inventory:
-            if item.condition > highest_condition and string == item.get_category():
-                highest_condition = item.condition
-                best_item = item
-        return best_item
-
-    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
-            my_best_item = self.get_best_by_category(their_priority)
-            their_best_item = other_vendor.get_best_by_category(my_priority)
-            # if my_best_item and their_best_item:
-            #     self.swap_items(other_vendor, my_best_item, their_best_item)
-            #     return True
-            # return False
-    
-            return self.swap_items(other_vendor, my_best_item, their_best_item)
-    def get_by_category(self, category):
-        items_in_category = []
-        for item in self.inventory:
-            if item.get_category() == category:
-                items_in_category.append(item)
-        return items_in_category
-
-
     def get_best_by_category(self, category):
         if not self.get_by_category(category):
             return None
@@ -87,21 +50,17 @@ class Vendor:
         item_list = self.get_by_category(category)
         best_condition = 0
         best_item = item_list[0]
-
         for item in item_list:
             if item.condition > best_condition:
                 best_condition = item.condition
                 best_item = item
-        
         return best_item
-
+    
+      
     def swap_best_by_category(self, other_vendor, my_priority, their_priority):
-        if not self.get_best_by_category(their_priority) or not other_vendor.get_best_by_category(my_priority):
-            return False
-        
-        my_item_to_swap = self.get_best_by_category(their_priority)
-        their_item_to_swap = other_vendor.get_best_by_category(my_priority)
+            my_best_item = self.get_best_by_category(their_priority)
+            their_best_item = other_vendor.get_best_by_category(my_priority)
+    
+            return self.swap_items(other_vendor, my_best_item, their_best_item)
+    
 
-        self.swap_items(other_vendor, my_item_to_swap, their_item_to_swap)
-
-        return True
