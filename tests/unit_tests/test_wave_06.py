b4b1862b079505187c35caed49817e34893de8c7
Kathleen <kathleen.n.shin@gmail.com>
2023-04-07 01:10:00 -0700

    Edited assertions needing to be updated to include correct items in each inventory

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -113,12 +113,8 @@ def test_swap_best_by_category():
     assert result
     assert len(tai.inventory) == 3
     assert len(jesse.inventory) == 3
-    assert item_a in tai.inventory
-    assert item_b in tai.inventory
-    assert item_c in jesse.inventory
-    assert item_d in jesse.inventory
     assert item_f in tai.inventory
-    assert item_e in jesse.inventory
+    assert item_c in jesse.inventory
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
@@ -154,12 +150,8 @@ def test_swap_best_by_category_reordered():
     assert result is True
     assert len(tai.inventory) == 3
     assert len(jesse.inventory) == 3
-    assert item_a in tai.inventory
-    assert item_b in tai.inventory
-    assert item_c in jesse.inventory
-    assert item_d in jesse.inventory
-    assert item_f in tai.inventory
-    assert item_e in jesse.inventory
+    assert item_c not in tai.inventory
+    assert item_f not in jesse.inventory
 
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
@@ -245,15 +237,15 @@ def test_swap_best_by_category_no_match_is_false():
         their_priority="Clothing"
     )
 
-    assert result is True
+    assert result == False
     assert len(tai.inventory) == 3
     assert len(jesse.inventory) == 3
+    assert item_d in jesse.inventory
+    assert item_e in jesse.inventory
+    assert item_f in jesse.inventory
     assert item_a in tai.inventory
     assert item_b in tai.inventory
-    assert item_c in jesse.inventory
-    assert item_d in jesse.inventory
-    assert item_f in tai.inventory
-    assert item_e in jesse.inventory
+    assert item_c in tai.inventory
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
@@ -286,14 +278,14 @@ def test_swap_best_by_category_no_other_match_is_false():
         their_priority="Decor"
     )
 
-    assert not result
+    assert result == False
     assert len(tai.inventory) == 3
     assert len(jesse.inventory) == 3
     assert item_a in tai.inventory
     assert item_b in tai.inventory
-    assert item_c in jesse.inventory
+    assert item_f in jesse.inventory
     assert item_d in jesse.inventory
-    assert item_f in tai.inventory
+    assert item_c in tai.inventory
     assert item_e in jesse.inventory
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
