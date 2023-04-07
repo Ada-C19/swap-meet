ec889c70d1fad24797d0696fc1971f733c3a923f
Anh Huynh <avh2@uw.edu>
2023-04-07 07:25:41 -0700

    Added comments to code

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,19 +3,27 @@ from swap_meet.item import Item
 
 
 class Clothing(Item):
+    # Define attributes for Clothing instances
     def __init__(self, id = None, fabric = None, condition = None): 
-        super().__init__(id, condition)       
+        # User inheritance logic to inherit certain attributes of Item class
+        super().__init__(id, condition)   
+        # If no value provided for fabric, attribute fabric takes on "Unknown"    
         if fabric is None:
             self.fabric = "Unknown"
         else:
             self.fabric = fabric
         
     def get_category(self):
+        # Return class name "Clothing"
         return Clothing.__name__
     
+    # Stringify Clothing instances
     def __str__(self):
+        # Read id attribute
         id_value = self.id
+        # Read fabric attribute
         fabric_value = self.fabric
+        # Return expected string
         return f"An object of type Clothing with id {id_value}. It is made from {fabric_value} fabric."
         
 
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,24 +1,32 @@
 import uuid
 
 class Item:
+    # Define attributes for Item instances
     def __init__(self, id = None, condition = None):
+        # If no value provided for id, attribute id takes on randomly generated unique number
         if id is None:
             self.id = int(uuid.uuid4())
         else:
             self.id = id
-            
+        
+        # If no value provided for condition, attribute condition takes on 0    
         if condition is None:
             self.condition = 0
         else:
             self.condition = condition
             
     def get_category(self):
+        # Return class name "Item"
         return Item.__name__
     
+    # Stringify Item instances
     def __str__(self):
+        # Read id attribute
         item_id = self.id
+        # Return expected string
         return f"An object of type Item with id {item_id}."
     
+    # Read condition attribute and return a string corresponding to attribute value
     def condition_description(self):
         if 0 <= self.condition < 1:
             return "fair"
