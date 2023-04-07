5a9dbe7d421678c7aa0b5218906170b57fcdcadb
Allie Soliz <solizale@gmail.com>
2023-04-07 03:39:52 -0400

    refactored Decor class to inherit from parent class Item

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,13 +1,11 @@
 import uuid
+from swap_meet.item import Item
 
 
-class Decor:
+class Decor(Item):
 
     def __init__(self, id=None, width=0, length=0, condition=0):
-        if id:
-            self.id = id
-        else:
-            self.id = int(uuid.uuid4())
+        super().__init__(id)
         self.width = width
         self.length = length
         self.condition = condition
@@ -17,17 +15,3 @@ class Decor:
 
     def __str__(self):
         return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
-
-    def condition_description(self):
-        if self.condition == 0:
-            return "cannot reuse"
-        elif self.condition == 1:
-            return "poor"
-        elif self.condition == 2:
-            return "heavily used"
-        elif self.condition == 3:
-            return "good"
-        elif self.condition == 4:
-            return "like-new"
-        elif self.condition == 5:
-            return "new"
