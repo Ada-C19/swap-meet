5f0364cacbd98a8d42e4f68934f20976690c6f43
Sarah Kim <kimsuyon2013@gmail.com>
2023-04-06 20:10:41 -0700

    fixed parameter order for __init__

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -2,7 +2,7 @@ from swap_meet.item import Item
 
 
 class Decor(Item):
-    def __init__(self, width=0, length=0, id=None, condition=0):
+    def __init__(self, id=None, condition=0, width=0, length=0):
         super().__init__(id, condition)
         self.width = width
         self.length = length
