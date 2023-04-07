da4a1b489d7882e431fa232c3d1248a8d824fa61
Yulia S <startyupp@gmail.com>
2023-04-06 20:03:48 -0700

    Added edge cases conditional statements to each subclass constructor, so if arguments are not as expected we will get an error.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,10 +1,12 @@
 from .item import Item
 
 class Decor(Item):
-    def __init__(self, id=None, width=0, length=0, condition=0):
-        super().__init__(id=id, condition=condition)
-        self.width = width
-        self.length = length
+    def __init__(self, year=None, id=None, width=0, length=0, condition=0):
+        super().__init__(year=year, id=id, condition=condition)
+        self.width = width if width==0 else float(width)
+        self.length = length if length==0 else float(length)
+        if width < 0 or length < 0:
+            raise ValueError
         
     def __str__(self):
         return f'{super().__str__()} It takes up a {self.width} by {self.length} sized space.'
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,9 +1,11 @@
 from .item import Item
 
 class Electronics(Item):
-    def __init__(self, id=None, type=None, condition=0):
-        super().__init__(id=id, condition=condition)
-        self.type = 'Unknown' if type is None else type
+    def __init__(self, year=None, id=None, type='Unknown', condition=0):
+        super().__init__(year=year, id=id, condition=condition)
+        if not isinstance(type, str):
+            raise ValueError
+        self.type = type
         
     def __str__(self):
         return f'{super().__str__()} This is a {self.type} device.'
