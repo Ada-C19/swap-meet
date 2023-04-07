55f9676a321acddd2c47ee128cfd7d887386754c
Sophia Tran <sophiat8832@gmail.com>
2023-04-06 18:39:53 -0700

    refactored swap_best_by_category

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -58,16 +58,10 @@ class Vendor:
         return best_item
     
     def swap_best_by_category(self, other_vendor, my_priority, their_priority):
-        if other_vendor.get_by_category(my_priority):
-            their_best_item = other_vendor.get_best_by_category(my_priority)
-        else:
-            return False
-        
-        if self.get_by_category(their_priority):
-            my_best_item = self.get_best_by_category(their_priority)
-        else:
-            return False
-        
-        self.swap_items(other_vendor, my_best_item, their_best_item)
-        return True
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other_vendor.get_best_by_category(my_priority)
+        if my_best_item and their_best_item:
+            self.swap_items(other_vendor, my_best_item, their_best_item)
+            return True
+        return False
         
