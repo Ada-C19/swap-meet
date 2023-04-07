a48e736a93bb02f9fc29d5eb32a4f76e9e9c0669
Kira Sidhu <kira.kaur.sidhu@gmail.com>
2023-04-07 11:12:24 -0700

    Reformat

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -21,15 +21,18 @@ class Item:
 
     def condition_description(self):
         """This function returns a string depending on the condition of the item"""
-        if self.condition == 0:
-            return "Wear a hazmat suit for pickup"
-        elif self.condition == 1:
-            return "Ask your Mom to come pick you up, you will be scared"
-        elif self.condition == 2:
-            return "This just needs some tender-lovin' care"
-        elif self.condition == 3:
-            return "Meh, it's okay, might need a wash"
-        elif self.condition == 4:
-            return "Good find, just one or two problems with this one"
-        elif self.condition == 5:
-            return "Perfect condition! Someone probably accidently gave this away, but I guess you got lucky"
+        if isinstance(self.condition, int) or isinstance(self.condition, float):
+            if self.condition == 0:
+                return "Wear a hazmat suit for pickup"
+            elif self.condition == 1:
+                return "Ask your Mom to come pick you up, you will be scared"
+            elif self.condition == 2:
+                return "This just needs some tender-lovin' care"
+            elif self.condition == 3:
+                return "Meh, it's okay, might need a wash"
+            elif self.condition == 4:
+                return "Good find, just one or two problems with this one"
+            elif self.condition == 5:
+                return "Perfect condition! Someone probably accidently gave this away, but I guess you got lucky"
+        else:
+            print("Condition must be numerical")
