d527d5b8032a5f2c426c42de1f61ecb2ee981a87
Kathleen <kathleen.n.shin@gmail.com>
2023-04-06 23:07:43 -0700

    Created decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,18 @@
+from swap_meet.item import Item
+
+import uuid
+
 class Decor:
-    pass
+    def __init__(self, id=None, width=0, length=0):
+        if id:
+            self.id = id
+        else:
+            self.id = uuid.uuid4().int
+        self.width = width
+        self.length = length
+    
+    def get_category(self):
+        return "Decor"
+    
+    def __str__(self):
+        return (f"An object of type Decor with id { self.id }. It takes up a { self.width } by { self.length } sized space.")
