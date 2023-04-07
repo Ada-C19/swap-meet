2f18e3a06c4ce8b95819642d63f51030bbe3e0a7
Allie Soliz <solizale@gmail.com>
2023-04-07 03:40:18 -0400

    refactored Electronics class to inherit from parent class Item

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,13 +1,11 @@
 import uuid
+from swap_meet.item import Item
 
 
-class Electronics:
+class Electronics(Item):
 
     def __init__(self, id=None, type="Unknown", condition=0):
-        if id:
-            self.id = id
-        else:
-            self.id = int(uuid.uuid4())
+        super().__init__(id)
         self.type = type
         self.condition = condition
 
@@ -16,17 +14,3 @@ class Electronics:
 
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
-
-    def condition_description(self):
-        if self.condition == 0:
-            return "cannot reuse"
-        elif self.condition == 1:
-            return "poor"
-        elif self.condition == 2:
-            return "heavily used"
-        elif self.condition == 3:
-            return "good"
-        elif self.condition == 4:
-            return "like-new"
-        elif self.condition == 5:
-            return "new"
