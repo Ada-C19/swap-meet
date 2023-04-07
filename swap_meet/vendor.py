0c1b2288f49bbe8fe681fc4defcb49c52f7170e6
Whitney <whitneyshake@gmail.com>
2023-04-07 09:31:42 -0700

    Final Push for Swap Meet Deleted Comments

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -3,16 +3,13 @@
 class Vendor:
     def __init__(self, inventory = None):
         if inventory is None:
-        #     self.inventory = []
-        # else:
-        #     self.inventory = inventory
             inventory = []
         self.inventory = inventory
         
 
-    # def vendor_has_inventory(self):
-    #     if len(self.inventory) > 0:
-    #         return self.inventory
+    def vendor_has_inventory(self):
+        if len(self.inventory) > 0:
+            return self.inventory
 
     def add(self, item):
         self.inventory.append(item)
